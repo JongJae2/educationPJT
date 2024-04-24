579a171e669cde79cfc18ca3a4744b17c04453fa
강지헌 <wlgjs0458@naver.com>
2024-04-24 09:57:58 +0900

    fix(fe): next build를 export로 변경

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1e3206cb200e264fdddb4dbb2e4772daebc839fdbc94b78316d74892a0de6fa2
-size 92
+oid sha256:c6656a4f593d0434955f9d2a645825c1b5ad2594f9bdb420cead7b046fa2b946
+size 133
