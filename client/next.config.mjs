1285ead84fac4ce11d481c8206f4dfe06b391d8f
강지헌 <wlgjs0458@naver.com>
2024-04-24 10:11:00 +0900

    fix(fe): next.config.mjs 빌드 설정 변경

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c6656a4f593d0434955f9d2a645825c1b5ad2594f9bdb420cead7b046fa2b946
-size 133
+oid sha256:1f727dfd6e9c06f439501efcda3c9fb0a6281f448ecb3082ffd7894e6f6a1790
+size 135
