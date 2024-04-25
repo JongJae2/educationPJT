a1e10ed360d78eaa9d51d4b1082f0b5c50b2482d
최정윤 <janet971015@naver.com>
2024-04-25 16:00:22 +0900

    init project

---
+++ discoveryservice/src/main/java/com/a508/discoveryservice/DiscoveryserviceApplication.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fd77a5dd6d0ee5f4b9235ec67de88145191387429175b1eb40db47382025e897
+size 433
---
+++ discoveryservice/src/test/java/com/a508/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d853e5d6134b0a1a974687478af1aab1a18d7adfaa81a9b1720753e220dedc18
+size 227
