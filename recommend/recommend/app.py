8d3fbb6feedda038cc05a5b3de0e90770a08ce04
강지헌 <wlgjs0458@naver.com>
2024-04-25 10:48:55 +0900

    feat(be): flask 프로젝트 포트 변경

--- recommend/recommend/app.py
+++ recommend/recommend/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:61f5a3ec212c838690226459808294ab30fa9318ea309362baccbb18fd9f5b6a
-size 206
+oid sha256:fb5b30e59517ba72e473593e9b1987a674e39e11adf9db1001fd0dba4a0dbcae
+size 231
