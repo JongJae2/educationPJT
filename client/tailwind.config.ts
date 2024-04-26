f874f2a0a6f28a77360c00d89fab0ed903cde065
권정훈 <dhtmxk8134@naver.com>
2024-04-26 10:42:15 +0900

    chore(fe): 폰트 및 색상 커스텀 설정
    Former-commit-id: 9256386c2dcd6e5ca600c660d29f16f84e180fc6

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,19 +1,3 @@
-import type { Config } from 'tailwindcss';
-
-const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+version https://git-lfs.github.com/spec/v1
+oid sha256:8917414b4ebd9544ce75706e08d1387f1ebceaed27935f0af4fe26798260b8f3
+size 775
