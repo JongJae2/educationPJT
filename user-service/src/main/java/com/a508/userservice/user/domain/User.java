1e2b771af0fd8466e29dc5ef3e9a3219e0eb52e8
최정윤 <janet971015@naver.com>
2024-04-26 12:55:23 +0900

    feat(be): getRoles 추가
    Former-commit-id: c8442e839b27bc880613a2ce66e2ad6a03a2d21d

--- user-service/src/main/java/com/a508/userservice/user/domain/User.java
+++ user-service/src/main/java/com/a508/userservice/user/domain/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f9c088ade15ae4a97ec89b60e770829b9299daeea8fe15a5db8737dd2cdb4b6b
-size 769
+oid sha256:76784068c00ad6bce39c10b07699b46ec406a96a33686b24dba16322113eecf4
+size 988
