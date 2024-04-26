76d6136e3d3d3af59bafc2c63dac80a623d3a5d6
이지원 <dlwldnjs950@naver.com>
2024-04-26 15:58:14 +0900

    design(fe): /about 페이지 상단 구성
    Former-commit-id: 5764a3f331bc5006484ce2a918e9b80b8e11b7c9

--- client/src/app/(intro)/about/page.tsx
+++ client/src/app/(intro)/about/page.tsx
@@ -1,10 +1,3 @@
-import Header from '@/app/_common/Header';
-
-export default function Page() {
-  return (
-    <div>
-      <Header />
-      <div className='bg-yellow-400'>서비스 소개 페이지 정보</div>
-    </div>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:11b09b62fc40f810a6d9d3b63db3674c0805fd644bd0798fc3d1210fc56e517f
+size 1366
