091fe38290cd6659b235aee5330817d8a070337e
이지원 <dlwldnjs950@naver.com>
2024-04-26 17:11:25 +0900

    design(fe): /about 페이지 - card 이동
    Former-commit-id: 54d0ee00e81c4652486c645076c4644f72e484ff

--- client/src/app/(intro)/about/page.tsx
+++ client/src/app/(intro)/about/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:11b09b62fc40f810a6d9d3b63db3674c0805fd644bd0798fc3d1210fc56e517f
-size 1366
+oid sha256:5a6a778f224849a2807376f56d39fa1b99e1eef812daf9c2877db63245cc1741
+size 1538
