0adc5d9b2af7602de78cb18d985f7fe397ea2143
권정훈 <dhtmxk8134@naver.com>
2024-04-26 17:02:37 +0900

    chore(fe): 페이지 별 메타데이터 설정
    Former-commit-id: 6ca0abe52e341cdd459be8a49df2447ace042946

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e811f38a114f78a77ec5b44fc9baf42d4fa84a5414a4e6ac15d2d71d2db38c98
-size 176
+oid sha256:33820051527648a57f7909f05487bea9acda5a0719dc7d4083b635773250f716
+size 337
