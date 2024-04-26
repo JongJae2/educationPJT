65dbbb8da0a30415ee5b406dc907cfea28791816
최정윤 <janet971015@naver.com>
2024-04-26 12:53:29 +0900

    feat(be): 구글로그인 data 클래스 생성

---
+++ user-service/src/main/java/com/a508/userservice/login/data/GoogleOAuthTokenRes.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6c3f38e43c6273d4eed0a0c06345be54bd8158cfef5afdc5c8a1d537841ec3e1
+size 327
---
+++ user-service/src/main/java/com/a508/userservice/login/data/OauthTokenRes.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3f96345d92e1508c05b43b7d5dc6d2a2584c7128589d4b303656c32731aabb05
+size 450
