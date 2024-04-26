9aa42fe7557970e631f67732c2cd21e4a4fd40ec
최정윤 <janet971015@naver.com>
2024-04-26 09:27:25 +0900

    feat(be): User 엔티티 생성
    Former-commit-id: 619794b4580a0c5417c3d86c6706cd358da4795b

--- user-service/src/main/java/com/a508/userservice/user/domain/User.java
+++ user-service/src/main/java/com/a508/userservice/user/domain/User.java
@@ -1,8 +1,3 @@
-package com.a508.userservice.user.domain;
-
-import com.a508.userservice.common.entity.BaseEntity;
-import jakarta.persistence.Entity;
-
-@Entity
-public class User extends BaseEntity {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:f9c088ade15ae4a97ec89b60e770829b9299daeea8fe15a5db8737dd2cdb4b6b
+size 769
