4fb7627b1c4d9d68e5985bee6274fbb58f5825a5
권정훈 <dhtmxk8134@naver.com>
2024-04-26 12:41:12 +0900

    design(fe): css 속성 수정
    Former-commit-id: c98d3bdbffc76cb407dd8bc476501e1462ea072a

--- client/src/app/(intro)/login/page.tsx
+++ client/src/app/(intro)/login/page.tsx
@@ -1,3 +1,3 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a3e540890bdfca555fd9ab870b27608a66b0fc84cf606a71914a25f42c4f4b75
-size 102
+export default function Page() {
+  return <div className='bg-yellow-400'>로그인 페이지</div>;
+}
--- client/src/app/example/detail/page.tsx
+++ client/src/app/example/detail/page.tsx
@@ -1,3 +1,3 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9090fd832ea1d942054fa2c531327ab586eb429f275e1c15ac8faebb9d3bddc9
-size 106
+export default function ExampleDetail() {
+  return <main className='bg-yellow-400'>예시상세</main>;
+}
