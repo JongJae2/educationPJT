6709d18066eea41ce8f5dff18c595e9e599af525
하재률 <wofbf2@gmail.com>
2024-04-29 10:20:48 +0900

    design(fe): 분석 보고서 UI 구현
    Former-commit-id: 3bd24d754e96004593cce49a0530a1163d8b6a84

--- client/src/app/(main)/mypage/analysis/page.tsx
+++ client/src/app/(main)/mypage/analysis/page.tsx
@@ -1,3 +1,3 @@
-export default function Page() {
-  return <div className='bg-yellow-400'>분석 페이지</div>;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:75ec2a09491a2ce609b42d1866932f611bdc06afca02fde3fa03c83470d0e4cb
+size 27798
