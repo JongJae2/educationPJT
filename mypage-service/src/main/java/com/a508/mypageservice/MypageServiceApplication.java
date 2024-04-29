f1d4046f827f4657da197363bbd3c8fccf9af474
강지헌 <wlgjs0458@naver.com>
2024-04-29 14:24:55 +0900

    add(be): Dockerfile 추가
    Former-commit-id: 636afbd8b1a4a6b63c8beb72ada7465f5b0bc83e

--- mypage-service/src/main/java/com/a508/mypageservice/MypageServiceApplication.java
+++ mypage-service/src/main/java/com/a508/mypageservice/MypageServiceApplication.java
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0f1d1400b241eedfa654778999b3750e9b78633b48c6139c37f5bf5f171df96a
-size 329
+package com.a508.mypageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypageServiceApplication.class, args);
+	}
+
+}
