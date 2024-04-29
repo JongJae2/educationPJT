ec9c44a9383f739467a718722a5f4a724a347c07
이지원 <dlwldnjs950@naver.com>
2024-04-29 14:13:05 +0900

    design(fe): 소개페이지 default경로, hover 효과
    Former-commit-id: f9a604f42492493ffd8f0f5381d8aa1a12631fa4

---
+++ client/src/app/(intro)/(about)/_component/SituationCard.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:87d0a5dc23fdb210b36174884741d0364e5a0d8654b25633cc85adbda0e1af23
+size 533
---
+++ client/src/app/(intro)/(about)/page.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:094db94912322d86376cf5fc7bcf89bb2b5b92136bf65167b76126f7f11f190f
+size 2623
