57db1e6bab11162725b472a6495eb6750c4389b7
강지헌 <wlgjs0458@naver.com>
2024-04-29 14:23:22 +0900

    init mypage

---
+++ mypage-service/src/main/java/com/a508/mypageservice/MypageServiceApplication.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:0f1d1400b241eedfa654778999b3750e9b78633b48c6139c37f5bf5f171df96a
+size 329
---
+++ mypage-service/src/test/java/com/a508/mypageservice/MypageServiceApplicationTests.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6bbb151ba907cb7ef962a592ebd7b11f564f395061f02c0f0c85a69d1813ce45
+size 221
