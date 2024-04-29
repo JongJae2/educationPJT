0e1641e3fd20006121ba7fe7764c40a1ae68365b
littlebird <dlwldnjs950@naver.com>
2024-04-29 23:20:46 +0900

    design(fe): 삽질
    Former-commit-id: 3a4e57d9950d1c05244ada9efbca97099cf15f37

--- client/src/app/(intro)/(about)/_component/IntroduceWrapper.tsx
+++ client/src/app/(intro)/(about)/_component/IntroduceWrapper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba45617a78fd0feb11c34bc8a719b7e0a4299a6795a94dbe987b395501f77d42
-size 1434
+oid sha256:345aabb8f3396b9e9558f2aa537cfd41ce7a6efdac63bad6a0a19ff47bf9bbe6
+size 1531
