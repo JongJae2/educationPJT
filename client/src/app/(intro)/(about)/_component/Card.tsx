47d03c76f349ea94f1fdf208f5e84fe0c436e5b8
이지원 <dlwldnjs950@naver.com>
2024-04-30 15:09:21 +0900

    remove(fe): 미사용 파일 삭제
    Former-commit-id: 51028798f35a9774621fffa3fed8869ecebe4f2e

--- client/src/app/(intro)/(about)/_component/Card.tsx
+++ client/src/app/(intro)/(about)/_component/Card.tsx
@@ -1,3 +1,18 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bc6d4de69d17e2f345aa95432fe8daf6fba6263310686a272b8ad0bfd3e2848f
-size 478
+import Image from 'next/image';
+
+export default function Card({ num }: { num: number }) {
+  return (
+    <Image
+      src={`/about/card${num}.png`}
+      loading='eager'
+      width={500}
+      height={500}
+      data-w-id='82fb0c48-ee0d-a227-b693-79ad41998fe6'
+      alt=''
+      className='w-1/5 drop-shadow-[2px_2px_2px_rgba(0,0,0,0.2)]'
+    />
+    // <div className='w-1/3 bg-black'>
+    //   <div className='bg-green-500/50 w-[200px] h-[200px]'></div>
+    // </div>
+  );
+}
