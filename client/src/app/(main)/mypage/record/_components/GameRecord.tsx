9f12ff4e3f2c1065fe03616d719aa1328cbcced6
이지원 <dlwldnjs950@naver.com>
2024-04-30 11:25:15 +0900

    design(fe); 마이페이지 - 학습기록 component 분리
    Former-commit-id: ac706844765d1de346e26b786a708f4f656f35dc

---
+++ client/src/app/(main)/mypage/record/_components/GameRecord.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:63975a21a624941a09e3e173817e2fb2017e6dd37df17e02e3e44a9240d7defa
+size 2564
---
+++ client/src/app/(main)/mypage/record/_components/StudyRecord.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:63a1b46a3dd36ce9c9443c0f71ba1a3097072d1c0978766e43e5c385108f7e6e
+size 2709
