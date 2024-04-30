8cebb6fb6158152194ce45256c2825c3acf18de4
이지원 <dlwldnjs950@naver.com>
2024-04-30 15:42:26 +0900

    design(fe): 소개페이지 헤더 변경
    Former-commit-id: 47033dea737d9b569fb95f448258406bf0772399

--- client/src/app/_common/QuizButton.tsx
+++ client/src/app/_common/QuizButton.tsx
@@ -1,7 +1,3 @@
-export default function QuizButton() {
-  return (
-    <div>
-      <button>간편 테스트</button>
-    </div>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:73369c393d76a36d20ea29e3455749ef840a2ce32d2a0399173b2354bd98dc2c
+size 247
--- client/src/app/_common/StartButton.tsx
+++ client/src/app/_common/StartButton.tsx
@@ -1,7 +1,3 @@
-export default function StartButton() {
-  return (
-    <div>
-      <button>시작하기</button>
-    </div>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:63081a84b511fa4b8832f8d992a51bee112517121e4a5d5e5e5740c390c8b145
+size 246
