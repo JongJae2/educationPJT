6b1d0ff3dda4bb27104d0cf7dd3f0d5fff6d73d1
이종재 <149658209+JongJae2@users.noreply.github.com>
2024-05-01 13:37:22 +0900

    feat-BE : 오늘의 학습, 주관식, 객관식, 오답 정답 확인
    Former-commit-id: 4133308a2df9d6159d6b952034b8f46348c99f3b

---
+++ study-service/src/main/java/com/a508/studyservice/entity/Essay.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:dcfb9072b3ace302f4dea71b5f65738bb5a307facd81ef57cc3cf350e4f70987
+size 743
---
+++ study-service/src/main/java/com/a508/studyservice/entity/TodayLearning.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3cc5a854be0cf46ba8a9ffcf11635d74c288313c71f002b617cc8a7e3bad2beb
+size 578
