9903c45028d4c36179372fcbdbc1d4a6d0fa6121
이종재 <ljj452@naver.com>
2024-05-01 19:27:01 +0900

    feat-BE : 문장 삽입  service,repo 생성

---
+++ study-service/src/main/java/com/a508/studyservice/respository/SentenceInsertRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:555e86ff22cd3aa595ff612cb65f6a96947024411dd5cc7c6e29a0e9a77dce06
+size 313
---
+++ study-service/src/main/java/com/a508/studyservice/service/SentenceInsertService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c142bfbf7869c6dda4442a5b99fe127125e86ea89dcc662da75f32ce99a5ae5f
+size 435
