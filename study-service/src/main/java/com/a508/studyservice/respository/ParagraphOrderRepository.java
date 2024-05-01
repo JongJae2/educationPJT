ea6ecae45110e1fd74ed937dad96bab264b94104
이종재 <ljj452@naver.com>
2024-05-01 19:26:33 +0900

    feat-BE : 문장 순서  service,repo 생성

---
+++ study-service/src/main/java/com/a508/studyservice/respository/ParagraphOrderRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:90cc08247cfa8d1bb77caf5895132956fa10acb4596b9a8397b9c2fb078c6115
+size 313
---
+++ study-service/src/main/java/com/a508/studyservice/service/ParagraphOrderService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6a87d195b31961479a9621bf7a75a7086f6c394f773dfe253a833d2b259edc9f
+size 433
