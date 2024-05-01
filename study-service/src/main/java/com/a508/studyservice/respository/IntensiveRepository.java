b3ff88516504ecb3e68b595f241de2a9cf974b06
이종재 <ljj452@naver.com>
2024-05-01 19:26:22 +0900

    feat-BE : 정독  service,repo 생성

---
+++ study-service/src/main/java/com/a508/studyservice/respository/IntensiveRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8f31ff2a1ff0201f72b7d7b4cc01fa00984726801cb58d932e98c37238e6dbee
+size 298
---
+++ study-service/src/main/java/com/a508/studyservice/service/IntensiveService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c9bc80a9e315db87e079bf638e50c16774eade61d57296ee7c0dd014a122a2f4
+size 415
