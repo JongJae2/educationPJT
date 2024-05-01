75c4a13260ced472ec9e7067e77c7b843125f4f6
이종재 <ljj452@naver.com>
2024-05-01 19:27:56 +0900

    feat-BE : 주관식 service,repo 생성

---
+++ study-service/src/main/java/com/a508/studyservice/respository/TopicRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b273a7d02c58fe66198bb158c1062259e807f8ccd0744b819ff34884aa20d02f
+size 302
---
+++ study-service/src/main/java/com/a508/studyservice/service/TopicProblemService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ff2bafe90d30597364465a0b4a5b8e4d966fc0dfad27c6e4ddab32b0d2814f8e
+size 403
