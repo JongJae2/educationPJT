e6216f22285b57e1c5a8535182d29d413e0cf131
이종재 <ljj452@naver.com>
2024-05-01 19:30:05 +0900

    fix-BE : 주관식 문제 topic 클래스 명 전달성 고려 수정

--- study-service/src/main/java/com/a508/studyservice/entity/Topic.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:02d3d6a3c7151b0785e9cee73d69ba16fc78bbda05ff71ccaecad5046903a604
-size 536
---
+++ study-service/src/main/java/com/a508/studyservice/entity/TopicProblem.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f9c8ab57221d2deaf06ce8fd4595b7671bc7d3e4cc827692dbb90a7b6848bfd6
+size 543
