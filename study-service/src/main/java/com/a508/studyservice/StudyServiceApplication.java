4a795c8f1b1b7858586f4dee2a8383383cf72d11
이종재 <149658209+JongJae2@users.noreply.github.com>
2024-05-01 13:32:42 +0900

    feat-BE : 스케쥴링, 날짜 감시, Feign 어노테이션 추가
    Former-commit-id: d0c6cb7ba9277cce955cec88d70b202f740fb366

--- study-service/src/main/java/com/a508/studyservice/StudyServiceApplication.java
+++ study-service/src/main/java/com/a508/studyservice/StudyServiceApplication.java
@@ -1,15 +1,3 @@
-package com.a508.studyservice;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-
-@SpringBootApplication
-// @EnableDiscoveryClient
-public class StudyServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(StudyServiceApplication.class, args);
-	}
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:ee2e8da111f0a8bd7b1f7ef16045f73117881c664e7f6d12796ba30443a85d7c
+size 691
