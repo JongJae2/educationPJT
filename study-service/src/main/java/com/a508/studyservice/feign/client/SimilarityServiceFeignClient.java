c19074743ddb0eed8223825820ba2836a0e73eea
이종재 <ljj452@naver.com>
2024-05-03 00:45:35 +0900

    rename(be): SOLID 원칙 준수 feign 수정
    Former-commit-id: 112f3cfb5c1fcf7013a5f1e4b6c766f461e7e37f

--- study-service/src/main/java/com/a508/studyservice/feign/client/SimilarityServiceFeignClient.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:958bf3110e3b93f68592f61e4e414146aa7406a949873fb7ec701fba93309c9e
-size 539
--- study-service/src/main/java/com/a508/studyservice/feign/client/UserServiceFeignClient.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c7ba7ab5d8915c1e264ecb9ab83fffe710fddddabe6a3a292954537c50eab9a8
-size 613
