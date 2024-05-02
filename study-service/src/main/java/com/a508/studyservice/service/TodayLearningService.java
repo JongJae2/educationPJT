bb969a0266a583489cf33bb2c673974b3e905096
이종재 <ljj452@naver.com>
2024-05-03 00:40:30 +0900

    rename(be): SOLID 원칙에 맞춘 폴더, 파일 변경
    Former-commit-id: a91020273e0917af664209e78de572d63e9c9eae

--- study-service/src/main/java/com/a508/studyservice/service/TodayLearningService.java
+++ study-service/src/main/java/com/a508/studyservice/service/TodayLearningService.java
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6446fd8a7fd82a4015e7ee1cb478f67cfcc492353a6b5837447d0dad831f2444
-size 522
+package com.a508.studyservice.service;
+
+public interface TodayLearningService {
+}
