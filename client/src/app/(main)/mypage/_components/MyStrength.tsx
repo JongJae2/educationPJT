f2e26304db223a5ca42fb26e908ac901e1453ebc
권정훈 <dhtmxk8134@naver.com>
2024-05-02 09:24:46 +0900

    chore(fe): import 경로 수정
    Former-commit-id: adced5a3b3f3b32ec98b363e0ae8dc43ed61fce3

--- client/src/app/(main)/mypage/_components/MyStrength.tsx
+++ client/src/app/(main)/mypage/_components/MyStrength.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a8412c32a2bb45f3a6ff038ed63d5af2a608b02bcae59cd32c69cf9c3c305187
-size 16893
+oid sha256:930592939d24fa3f4b6d8270610c9d81e6071cb07a186c134b240da48d26d75f
+size 16063
