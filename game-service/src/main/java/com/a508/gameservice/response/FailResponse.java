5a5a27286c24fed31bc2708e3b12ef6a2682c67e
최정윤 <janet971015@naver.com>
2024-05-02 16:48:01 +0900

    feat(be): response 관련 클래스 생성

---
+++ game-service/src/main/java/com/a508/gameservice/response/FailResponse.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a6d957adafd02980e1870ef14fbcc780fc61439c9238fcabac1604b55663a251
+size 472
---
+++ game-service/src/main/java/com/a508/gameservice/response/SuccessResponse.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:fcd320a41361a770bfad976c24e21b80ab48bc8419f185717d99a5832750e76f
+size 177
