b16fa90b8174e7f88e90640008ab9ba6a20b8d2d
권정훈 <dhtmxk8134@naver.com>
2024-05-02 10:54:08 +0900

    chore(fe): data 디렉터리 변경
    Former-commit-id: 42c4860c074bc000ce3b208c63f9bb9d62bf90c2

--- client/src/app/(intro)/quiz/solve/_components/Quiz.tsx
+++ client/src/app/(intro)/quiz/solve/_components/Quiz.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:89447af2fcfeece2325bc87bf952d068ae79ecaa7fefb48930d5ba488ab21c63
-size 3290
+oid sha256:546180d3910bb006b67a23c0100ffb26d2f1521bbebce8ef10b0c793340138c5
+size 3298
