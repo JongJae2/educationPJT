fcff26064ab6017fc3276104d1be33587bfd7767
권정훈 <dhtmxk8134@naver.com>
2024-05-02 09:24:46 +0900

    chore(fe): import 경로 수정
    Former-commit-id: c972b432ba0983ea7b3dca9b15eba3e5264babad

--- client/src/app/(main)/mypage/_components/MyGraph.tsx
+++ client/src/app/(main)/mypage/_components/MyGraph.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f4f95ee9be8b434d14bf790f27340dd65e96d6da3193a22b91f6cb695c972c86
-size 2699
+oid sha256:ad7e5339fdad13f1357475c1a83df8ff2f9bbefe792cfbb421d4ecd07a0c6687
+size 2677
