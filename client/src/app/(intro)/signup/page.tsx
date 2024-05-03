8ec3ae0a41403d3f859ff5918756a071e3ad8e3d
이지원 <dlwldnjs950@naver.com>
2024-05-03 18:06:50 +0900

    design(fe): 한지 배경
    Former-commit-id: 913186e8137246686a6fe44accebd094b7ed0d9e

--- client/src/app/(intro)/signup/page.tsx
+++ client/src/app/(intro)/signup/page.tsx
@@ -1,3 +1,3 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6929a05cf6dc6a29878a33343861c75537d6179f924873443ec3cf54896fd27e
-size 105
+export default function Page() {
+  return <div className='bg-yellow-400'>회원가입 페이지</div>;
+}
