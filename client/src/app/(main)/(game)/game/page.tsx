69ed7a699da8e333f7c5f886c25f27a8b5af55cd
하재률 <wofbf2@gmail.com>
2024-05-03 16:37:35 +0900

    feat(fe): 마이페이지-분석보고서 API 연동
    Former-commit-id: c0839f640fbe4f950f2275ea0dd157e0a28dee05

--- client/src/app/(main)/(game)/game/page.tsx
+++ client/src/app/(main)/(game)/game/page.tsx
@@ -1,3 +1,3 @@
-export default function Page() {
-  return <div className='bg-yellow-400'>게임 페이지</div>;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:48254f55054d0e119e9b8713441312bf77bc67abc1b0e86214a5a962b8351f9e
+size 99
--- client/src/app/(main)/(nav)/home/page.tsx
+++ client/src/app/(main)/(nav)/home/page.tsx
@@ -1,10 +1,3 @@
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: '심심한 사과, 당신의 문해력 지키미',
-  description: '문해력 학습 서비스 심심한 사과의 오늘의 문해력 학습',
-};
-
-export default function Page() {
-  return <div className='bg-yellow-400'>홈 페이지</div>;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:612aa813893e1012319ddf45ea32aad7fbdb7d75c56aeae1cd6f732d7e9e43c8
+size 327
