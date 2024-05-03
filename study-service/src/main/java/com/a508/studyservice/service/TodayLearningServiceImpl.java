f6a1f0fd014693d163a2a4e965f4bfbb863053a1
이종재 <ljj452@naver.com>
2024-05-03 17:53:54 +0900

    refactor(be): 어노테이션 수정
    Former-commit-id: 1f550b9bd5a1995b22dd785d0a0417c226f76471

--- study-service/src/main/java/com/a508/studyservice/service/TodayLearningServiceImpl.java
+++ study-service/src/main/java/com/a508/studyservice/service/TodayLearningServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c0bf5be910b5ab027178853d51c2dc1829dcc422ff64862f4191e51a0dcf9ce
-size 519
+oid sha256:59378817a28b5aeeb8791b9e9450f8faf226a8a3c375b4c5bc4d3221d112b040
+size 518
