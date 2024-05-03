0a4a25a5ffb8d05c1f2e1973e721cdb9496e65cc
이지원 <dlwldnjs950@naver.com>
2024-05-03 19:04:32 +0900

    design(fe): 선택 날짜 출력, 달력 CSS 시작
    Former-commit-id: 725c530bf44fd4752936773457dcb64aa9bcd1b3

--- client/src/app/(main)/(nav)/mypage/record/_components/MonthlyRecord.tsx
+++ client/src/app/(main)/(nav)/mypage/record/_components/MonthlyRecord.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9da3a62f3c921a4b7f7e351a9d79f46a6e9a90c870bfd7afdc9326c3d35c85ee
-size 1869
+oid sha256:6e58d631c63977ad1014c98d1dc2da26a91f79f0573c26acb7144eeb25340bef
+size 1891
