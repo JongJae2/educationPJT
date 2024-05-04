f90879958ee53185ab69846dca2829eb55a40539
이종재 <ljj452@naver.com>
2024-05-05 01:58:11 +0900

    style(be): dto 변수명 변경
    Former-commit-id: d9de5836948791e77f782bd279ef6e1dd6ad4f35

--- study-service/src/main/java/com/a508/studyservice/dto/response/ProblemResponse.java
+++ study-service/src/main/java/com/a508/studyservice/dto/response/ProblemResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c07b1199ba3beb0cf836a151a2d1d1a8e0bdea463355c5083f80fbe3ce16d1b
-size 601
+oid sha256:f53a8cdb1d2b12f283d8ce2ccc4969bf4584c4ba83ccf1f704407701537b7623
+size 627
