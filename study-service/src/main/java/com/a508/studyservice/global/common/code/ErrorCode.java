fe363a901b46f99aeb1d5b60196e753109a72e0f
이종재 <ljj452@naver.com>
2024-05-05 01:57:37 +0900

    feat(be): ERROR CODE 추가
    Former-commit-id: bca0bf906d6df1159309530eab844d8f7f07a9fd

--- study-service/src/main/java/com/a508/studyservice/global/common/code/ErrorCode.java
+++ study-service/src/main/java/com/a508/studyservice/global/common/code/ErrorCode.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9de2d22077cd9d45f56d01bc37416a516ec56261a78a9feb7ce68ccc186b30a8
-size 2543
+oid sha256:693e90e304fe1fa96cb0a895bdb50a467877feecedd8675033b5e04ca00f1d49
+size 2657
