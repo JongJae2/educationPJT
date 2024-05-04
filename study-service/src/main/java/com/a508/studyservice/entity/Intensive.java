732aeb9af6bc9b827006b408edbe364eab5ddc50
이종재 <ljj452@naver.com>
2024-05-05 01:54:09 +0900

    fix(be):  엔티티 컬럼 추가
    Former-commit-id: d3eb992f9edc430fec88d7b9c7e94aee199ba241

--- study-service/src/main/java/com/a508/studyservice/entity/Intensive.java
+++ study-service/src/main/java/com/a508/studyservice/entity/Intensive.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:934a3f0c55079f5e1c2388efc0a8302f761679a14b210646af840d0c7888eb23
-size 626
+oid sha256:218a9f4be3c6c73ae38c07d30510da6d9d1b9e3cd16f768af20757183632aba4
+size 656
