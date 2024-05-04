d49018c69c1334f24232ad98839c1f5a48476d83
이종재 <149658209+JongJae2@users.noreply.github.com>
2024-05-04 18:26:05 +0900

    feat(be): 유저 5가지 수치, 어휘 퀴즈 엔티티 생성
    Former-commit-id: f4e19e7b1f06a5468ab091db309aa49bd702c12d

---
+++ study-service/src/main/java/com/a508/studyservice/entity/FiveAbility.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9d807a209d7157fabd62281e3561b6ab22220bb55f14f48f02ce72133f472529
+size 425
---
+++ study-service/src/main/java/com/a508/studyservice/entity/Voca.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4697e3862a1aad56fcee65ab311111b9ded48c50e15262e164dc1160f29e8861
+size 571
