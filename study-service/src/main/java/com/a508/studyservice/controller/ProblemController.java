97be39601973cea37a96b1e0dd5afa7c3fa01d86
이종재 <ljj452@naver.com>
2024-05-05 02:28:48 +0900

    fix(be): token 선택값으로 수정
    Former-commit-id: 21603f0636e1b5733bc4e1aa838f670f1c17a682

--- study-service/src/main/java/com/a508/studyservice/controller/ProblemController.java
+++ study-service/src/main/java/com/a508/studyservice/controller/ProblemController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:565cff51f1f452ddc2aff06db3c73a8e743080893225e809bd8c321a3e59bfc0
-size 3078
+oid sha256:8c7f1980e759e60877f3c0ff1c77d9291cebcc3b92ad0916db8b8d2e29bf5f8b
+size 3103
