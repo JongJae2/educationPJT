4ea33b718531bbbaec9f161fae048c2944d0d5d5
이지원 <dlwldnjs950@naver.com>
2024-05-05 13:11:15 +0900

    style(fe): 달력 디자인
    Former-commit-id: 0569d473c68ea71605b106a24beaffd06eddb5b3

--- client/src/app/(main)/(nav)/mypage/record/_components/MonthlyRecord.tsx
+++ client/src/app/(main)/(nav)/mypage/record/_components/MonthlyRecord.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e58d631c63977ad1014c98d1dc2da26a91f79f0573c26acb7144eeb25340bef
-size 1891
+oid sha256:abae3e0d88210a9252b060772f006967d71a473456baefa3e3db543e80b862d7
+size 2096
