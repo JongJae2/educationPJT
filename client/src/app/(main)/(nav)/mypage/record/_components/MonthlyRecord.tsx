de8d866f2827316c1743b447bbecaf0db73f3c39
이지원 <dlwldnjs950@naver.com>
2024-05-05 13:53:11 +0900

    style(fe): 달력 선택 가능 기간 설정
    Former-commit-id: 83d7737f15abee23b69682065105a4999b2a01db

--- client/src/app/(main)/(nav)/mypage/record/_components/MonthlyRecord.tsx
+++ client/src/app/(main)/(nav)/mypage/record/_components/MonthlyRecord.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:abae3e0d88210a9252b060772f006967d71a473456baefa3e3db543e80b862d7
-size 2096
+oid sha256:d8dc865bd544f75062b624a5a3fe4f8cbe1f19b3aaaa29a9fae729b346aae132
+size 2158
