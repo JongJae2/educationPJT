424bbb48d32bd98b8c04cbb11aa774a682747d67
이지원 <dlwldnjs950@naver.com>
2024-05-05 23:51:42 +0900

    style(fe): 소개페이지 내용 채움 ing...
    Former-commit-id: fb5c8e43b5a122984355fff5dc7cd56aa236cc7a

--- client/src/app/(intro)/(about)/page.tsx
+++ client/src/app/(intro)/(about)/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b611f65a76a681cfe5706cce7e2d88c4244190aeda3f191f810ab4757112b72c
-size 4992
+oid sha256:7fc0ecd303a4e7a1f630400a75578c69956fe5a4b38bf5b33f35a7fee437363a
+size 6372
