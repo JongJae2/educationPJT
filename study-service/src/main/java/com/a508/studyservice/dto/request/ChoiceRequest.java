224cb7a547e9216682f9337f85289593bf5e9ff5
이종재 <149658209+JongJae2@users.noreply.github.com>
2024-05-06 22:07:30 +0900

    feat(be): 객관식, 주관식 RequestDto 추가
    Former-commit-id: 422e3df0b0e52fbdcab9c880b567ae6b1e6ad79f

---
+++ study-service/src/main/java/com/a508/studyservice/dto/request/ChoiceRequest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7f867f2434e2e84e29cc77b0bdf3c4814039f900d33a2f6a1c81661bfe3a9bcb
+size 203
---
+++ study-service/src/main/java/com/a508/studyservice/dto/request/EssayRequest.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7269ad455aa21bb2562e9791f261bcaf9f27381952abcf826cdf5629a628a2e7
+size 201
