2f8bab2edebe2bd8d78a48bbce5a71ead5b126d9
이종재 <149658209+JongJae2@users.noreply.github.com>
2024-05-06 22:07:04 +0900

    feat(be): 어휘 퀴즈 기능 추가
    Former-commit-id: 1031883d0b0adcb5506177f3fbef81c850c9f144

---
+++ study-service/src/main/java/com/a508/studyservice/repository/VocaRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d0401f148b0abe4efa92328e9df498a79a4cc393512786c61c36cf7f2de1f1db
+size 283
---
+++ study-service/src/main/java/com/a508/studyservice/service/VocaService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:58302048f6af739a64b96ada1809a92e7768415e92e5bc3e9889ed1191bc8a13
+size 266
