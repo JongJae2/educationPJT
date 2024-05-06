05e4b519ec1dcd77787ce45dc7b4938072a90240
이종재 <149658209+JongJae2@users.noreply.github.com>
2024-05-06 22:10:05 +0900

    feat(be) : 주제 맞추기 기능 구현
    Former-commit-id: 70fecafba346e8b540ce2398569b9d721fdbd823

--- study-service/src/main/java/com/a508/studyservice/entity/EssaySolved.java
+++ study-service/src/main/java/com/a508/studyservice/entity/EssaySolved.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b29a9a2b52befd024668186c47094b52b1fc5925ad5c2c309c275a9e2ca6f65
-size 749
+oid sha256:a4c7b26bb4fd7c5ad6e31d88aaa5b3a79505461a0f955d2b700d0cfc8e736d49
+size 782
