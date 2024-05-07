817c0e927160840b8050479d9cb3ef2da1d17525
최정윤 <janet971015@naver.com>
2024-05-08 06:23:29 +0900

    refactor(be): feign 요청을 통한 userId 반환 수정
    Former-commit-id: 31a9ad84f48ba95207f8ed5f84b1798e88c8024e

--- user-service/src/main/java/com/a508/userservice/common/jwt/TokenProvider.java
+++ user-service/src/main/java/com/a508/userservice/common/jwt/TokenProvider.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:580053e2ec19c902db369c198f56193ba7cf71a70d99b8142cfefbd96084b1ec
-size 4320
+oid sha256:4adac40acd6f7ae0a08b87cf8249ef85a331fe9888c6aee131c7035459cee26a
+size 4152
--- user-service/src/main/java/com/a508/userservice/user/controller/UserController.java
+++ user-service/src/main/java/com/a508/userservice/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8eb4fd7ec9a9094de22bafa9501f4cb2f3e15cea7c27ecf3b2368c65f0f534cb
-size 588
+oid sha256:ae1e200ef7660099af496efd350a060a367dd9dfd062ba807e737ae2ef765f34
+size 601
