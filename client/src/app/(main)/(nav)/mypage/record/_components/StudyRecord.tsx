76ea46dee11f4cdd3a74f86cd61c81527fa7912a
이지원 <dlwldnjs950@naver.com>
2024-05-07 09:32:25 +0900

    feat(fe): 월 변경 액션
    Former-commit-id: 03a669ea423f03424ecfb4c2e4155dd255dfe374

--- client/src/app/(main)/(nav)/mypage/record/_components/StudyRecord.tsx
+++ client/src/app/(main)/(nav)/mypage/record/_components/StudyRecord.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:63a1b46a3dd36ce9c9443c0f71ba1a3097072d1c0978766e43e5c385108f7e6e
-size 2709
+oid sha256:9490196d603bf4eadf48c955f0bbe8b4f31e0d77146f6d045e2b05932b4a17f0
+size 2791
