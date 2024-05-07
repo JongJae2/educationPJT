a0904cf481de738691e76d28e86b0646f5c3084c
이지원 <dlwldnjs950@naver.com>
2024-05-07 13:19:12 +0900

    design(fe): 방 목록 item
    Former-commit-id: 7eb7e8fe109ce5ab6f136c58fe2405ffb5088ca4

--- client/src/app/(main)/(game)/game/_component/GameRoomItem.tsx
+++ client/src/app/(main)/(game)/game/_component/GameRoomItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5bdc2b49d99c5c359961e51933fc97bd2a81df12562b9680da87fcd99a95836d
-size 4561
+oid sha256:4c20f202909542b17d03fad62abf759605c2042a1c824391bade8972a4096b98
+size 4694
