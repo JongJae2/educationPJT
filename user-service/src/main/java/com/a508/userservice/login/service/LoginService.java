0abb4ccce822cf913d5e6a132aa96a9568434411
최정윤 <janet971015@naver.com>
2024-05-07 13:02:11 +0900

    refactor(be): 배포 주소로 변경
    Former-commit-id: bb68a21a1f8a99abb3890bc17dedb3892c0d74b8

--- user-service/src/main/java/com/a508/userservice/login/service/LoginService.java
+++ user-service/src/main/java/com/a508/userservice/login/service/LoginService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9dfd8abaf19e4138ec69a8238d8d666af1e0f8bedfcd69c2aaf966c879000ad5
-size 7071
+oid sha256:085b3e79fe44e57d131e782b7ce318b9d67174e006ffa383744545361bcba71d
+size 7025
