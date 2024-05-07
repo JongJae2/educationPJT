5c459807903566bca2f43b7f0ff8a5ada5d13899
권정훈 <dhtmxk8134@naver.com>
2024-05-07 13:49:44 +0900

    fix(fe): 빌드 에러 수정 (useSearchParams를 위한 Suspense 추가)
    Former-commit-id: a1f8b590585c6bf8021fd9e7996bb62d0e3bbc07

--- client/src/app/(intro)/login/authentication/page.tsx
+++ client/src/app/(intro)/login/authentication/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75c0d80796b85a1291481216b7622edc6debfe824b535f95e344c1ae9dbbbef8
-size 704
+oid sha256:8489ce992c8d2c6984d925f93a02819aa0cd96c4480fc330fc5308b4a3a1fbdd
+size 830
