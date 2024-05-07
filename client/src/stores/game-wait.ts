ef8ef82fb1988b3d6119c2dbe464a492593eb8c0
이지원 <dlwldnjs950@naver.com>
2024-05-07 16:18:54 +0900

    design(fe): 비밀번호 입력 모달
    Former-commit-id: ec768f12b3507e3fb7ecc1d9dc368eb0c6b85d13

--- client/src/stores/game-wait.ts
+++ client/src/stores/game-wait.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0fd6872744f92b5bd3a35449129f989a416cd8a19769342672cf38fe409a395
-size 278
+oid sha256:d37fee50dd2333a80a5b2da59edcb9074fc79727cf4626aca9337debafbbdcdb
+size 693
