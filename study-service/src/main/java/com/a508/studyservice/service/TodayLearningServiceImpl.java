81f05d3f2c667126ac4d9f134a4e410693ae1b04
이종재 <ljj452@naver.com>
2024-05-07 17:48:06 +0900

    fix(be): 오늘의 학습 dto 변수명 수정
    Former-commit-id: fd9f9512ea5a2a2fb93bee4098e491f265bce269

--- study-service/src/main/java/com/a508/studyservice/service/TodayLearningServiceImpl.java
+++ study-service/src/main/java/com/a508/studyservice/service/TodayLearningServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4afe673c5e60cf83d80c5694d34ce5efc4b67f8e9401fd4ce5c2d1c4dc97c7e2
-size 3956
+oid sha256:681b48b8ead6fefd7a2fbafeab158ce6f90bb4e6cf820dc9f82b65a74e4371cc
+size 3954
