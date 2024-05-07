5f85016c409e5274ea9fa49f64b7268c754e52d3
이지원 <dlwldnjs950@naver.com>
2024-05-07 17:53:59 +0900

    design(fe): 게임방 ui 시작
    Former-commit-id: bcf92368f5dd5943a298145dc5099c3a249da56c

--- client/src/app/(main)/(game)/game/_component/GameRoomItem.tsx
+++ client/src/app/(main)/(game)/game/_component/GameRoomItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce7b7ea363df708df05e6bef44cf6feff2b7e0437a70cfe4d2ea39d2037c95d4
-size 5379
+oid sha256:8464dc904c7e11bbf5ce35c18f1e97a4c442626393f7f72f4c210f7487fdc941
+size 5244
