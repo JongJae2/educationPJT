572a9df650366e1f1c9ec9ca5ed0ab8a42b87b99
이지원 <dlwldnjs950@naver.com>
2024-05-07 14:52:55 +0900

    feat(fe): 잠긴 게임방 비밀번호 입력 모달
    Former-commit-id: 883a5fbdcf28119e8fac4fb44b7cc1f22bc99357

--- client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
+++ client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:92ee6cf1fcac633b72f970cf0749e477fe4549df8f20f147ff0dd5445f5a2cf5
-size 2620
+oid sha256:5f59fa87e003a9393d7d001cc5aa925cefe64cbb751dd4d52807b779595a7ea2
+size 2842
