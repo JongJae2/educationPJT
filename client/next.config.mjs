d8ac93715a8321d4496ea654264e6e937c7ee5c3
이지원 <dlwldnjs950@naver.com>
2024-05-07 13:43:55 +0900

    design(fe): 게임방 hover
    Former-commit-id: 2edeee205437143928bfc29b0a119b7934c952a5

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:662f447fb5cba248a2a9338d0d8680a9726080b945172a2889785da9886821a5
-size 116
+/** @type {import('next').NextConfig} */
+const nextConfig = { reactStrictMode: false };
+
+export default nextConfig;
