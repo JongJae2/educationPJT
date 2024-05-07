fb546e8ef42afae165ecebf6368d196fb64bad66
이지원 <dlwldnjs950@naver.com>
2024-05-07 13:19:12 +0900

    design(fe): 방 목록 item
    Former-commit-id: 691d21c3f08ff176fc5c2a2d91105e8286675b65

--- client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
+++ client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5528fd2fea1d0e11c480e3ad58e1041497e5f8cfb44b4da70d67330bd997b214
+oid sha256:0c8d13c52b1b34a3b5190c5e26fcf13dc1da552c675229f7b60bef2f8640568c
 size 2056
