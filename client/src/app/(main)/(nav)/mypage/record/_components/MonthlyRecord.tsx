a27ba237c048e3530846a56a4a63e657c97b2c7a
이지원 <dlwldnjs950@naver.com>
2024-05-07 10:29:44 +0900

    feat(fe): 학습목록 분리, 선택 월 표시
    Former-commit-id: 87120091e1b38c4be3631469b1b397fb9af78e71

--- client/src/app/(main)/(nav)/mypage/record/_components/MonthlyRecord.tsx
+++ client/src/app/(main)/(nav)/mypage/record/_components/MonthlyRecord.tsx
@@ -1,3 +1,19 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:387fa64167fba6896a82fabaca5839b4ec284a00e4b95cf2829c435fe1d15d28
-size 1778
+'use client';
+
+import CalendarComponent from './CalendarComponent';
+import RecordList from './RecordList';
+
+export default function MonthlyRecord() {
+  return (
+    <div className='flex flex-col p-5 w-full'>
+      <div className='flex justify-center'>
+        <div>
+          <CalendarComponent />
+        </div>
+      </div>
+      <div>
+        <RecordList />
+      </div>
+    </div>
+  );
+}
