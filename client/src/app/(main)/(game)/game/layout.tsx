88c13d3006aff09d2be57f636075796bd552e269
이지원 <dlwldnjs950@naver.com>
2024-05-08 09:05:54 +0900

    design(fe): 배경화면, 게임방 목록 변경
    Former-commit-id: b4ce5c1d78e3fb5f3facc9cadeffc5954bd643ae

--- client/src/app/(main)/(game)/game/layout.tsx
+++ client/src/app/(main)/(game)/game/layout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3ffb6bad9b84980674a21727333ec978b7b976e41f1f524364ab83e65f0e2a7
-size 818
+oid sha256:3bf3111dee146c9d723ea84e3d0a2662df911223f262d9e2714993f60c631d5a
+size 840
