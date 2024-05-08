57195b4917a51b491e9f3d30f597128f53515f3f
강지헌 <wlgjs0458@naver.com>
2024-05-08 16:41:49 +0900

    feat(be): req 파일 추가
    Former-commit-id: 19b5e0dfd903c771a8d98f2a7fa47058fe7c38a2

---
+++ user-service/src/main/java/com/a508/userservice/user/data/CategoryReq.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7f723fac476a71d82ec9a1e8c538c490cbfeef87f1d1d3830334f13ce5bee6b5
+size 191
---
+++ user-service/src/main/java/com/a508/userservice/user/data/NicknameReq.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e4ada9831ca9d15fa07299d100409ae2aded4124a1eedc1e33fe451e4be8b24e
+size 180
