37fde0f2cf37883f0a5d14b46f0ecb225adbe3f5
최정윤 <janet971015@naver.com>
2024-05-08 16:02:33 +0900

    feat(be): ranking 관련 dto 생성
    Former-commit-id: 451018c8bf031af0285a9e77ed9624416d11d870

---
+++ game-service/src/main/java/com/a508/gameservice/game/data/RankingRes.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3651a1d55be99de24f0ce21a243a0c35ca79c6dd7730476e435b4eb98b0a7341
+size 400
---
+++ game-service/src/main/java/com/a508/gameservice/game/data/TopRankingRes.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d0db1fde837e91cf17655841fdd436f000fb7cd88cb824638a08f3c38baf02e5
+size 328
