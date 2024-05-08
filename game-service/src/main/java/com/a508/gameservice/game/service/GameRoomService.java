cd6d3e7bbb6b83789f742d459a7c4070a1fdb717
최정윤 <janet971015@naver.com>
2024-05-09 00:59:34 +0900

    feat(be): 방 정보 반환 로직 구현
    Former-commit-id: 2968e6134f8c31c032dc0b034d1fe0492e7c9cd0

--- game-service/src/main/java/com/a508/gameservice/game/service/GameRoomService.java
+++ game-service/src/main/java/com/a508/gameservice/game/service/GameRoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:76afa8b6c8a3605d99f03e1358e6a71f352a5ce8c95144b2a1cc8101e609c0b0
-size 6410
+oid sha256:d575cc315051f3de2cd3bafd00298d81035c43a0b6b6a352a9f2460aedd15a0c
+size 8917
