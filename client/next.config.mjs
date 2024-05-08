910650dc7b2803ecf503d9a4c942dfda6b98ca8a
하재률 <wofbf2@gmail.com>
2024-05-08 13:56:59 +0900

    design(fe): 게임중 페이지 UI
    Former-commit-id: b5c275a3cf9c2a0c19a196e5f00a4d4011bb2a77

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,3 +1,6 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:729be5dc618aca1c3e0f90c139d06ed8a80e67e6f3faeb12f587ded29eec9168
-size 119
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+};
+
+export default nextConfig;
--- client/src/app/(intro)/layout.tsx
+++ client/src/app/(intro)/layout.tsx
@@ -1,3 +1,12 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a413d2bcdc082ca84d82c3561ca782a3b04ebdb1805913e94fd19c81daeca79f
-size 191
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      {/* 로그인 전 레이아웃 */}
+      {children}
+    </div>
+  );
+}
