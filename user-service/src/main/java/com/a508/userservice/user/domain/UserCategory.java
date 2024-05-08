17e6cab505fd09cfda054f2bb6150f2f6a389b24
강지헌 <wlgjs0458@naver.com>
2024-05-08 11:04:31 +0900

    feat(be): user 선호 category 테이블 추가
    Former-commit-id: cb6e56c6eeff9b3b05118a9e766cf91d237d288c

---
+++ user-service/src/main/java/com/a508/userservice/user/domain/UserCategory.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1c01fae422905228547fbe48ab3652e6e314e67651de2f02a0cd1065b5426eea
+size 434
---
+++ user-service/src/main/java/com/a508/userservice/user/repository/UserCategoryRepository.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:98a07f8b0c551910f95bdf4582921eb1a23f15341be983072d139eedc88feeff
+size 294
