408a91965cb6179fcb3e0e71411c282ef7e29719
최정윤 <janet971015@naver.com>
2024-05-09 00:59:59 +0900

    refactor(be): 반환값 수정
    Former-commit-id: 85af1235da040c3e17c1ec5e7434e0c28a072024

--- game-service/src/main/java/com/a508/gameservice/game/controller/GameRoomController.java
+++ game-service/src/main/java/com/a508/gameservice/game/controller/GameRoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:012ee94a7f08bb5d91aa0238a1fddfb8f199d7a87e7aeae387f9d892d6970b55
-size 1674
+oid sha256:71972c574264da37fa6ab1bb2b22b969f0424ab2e21383fa032efe1515f32588
+size 1410
