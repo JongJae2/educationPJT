4baabb4a801fca58010472f633bb12bd1fab9311
최정윤 <janet971015@naver.com>
2024-05-09 01:01:40 +0900

    feat(be): getPlayersInRoom 구현
    Former-commit-id: b87c45b5239b8b6d3d458c9dd3987e12393f0df7

--- game-service/src/main/java/com/a508/gameservice/game/repository/RoomPlayerRepository.java
+++ game-service/src/main/java/com/a508/gameservice/game/repository/RoomPlayerRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ae2292d849398e4dd5f7157a7e25a20a95d185a09c7a3a1bb37aff8c37211a01
-size 863
+oid sha256:2164182a7d957b5aa87e0a261a4a4ca3fed8ad6ef2203f8e369569de14f4b45d
+size 1086
