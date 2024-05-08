f91e05e46d7578ba94a6b85cfa22f379fd372333
강지헌 <wlgjs0458@naver.com>
2024-05-08 12:22:47 +0900

    fix(be): api endpoint 수정
    Former-commit-id: 464d9e564b4d8ed65056610721d17ed6b130ba84

--- user-service/src/main/java/com/a508/userservice/user/repository/UserRepository.java
+++ user-service/src/main/java/com/a508/userservice/user/repository/UserRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58d169f0be2d91f0ae4156786ab04acb884a91e75d754ac06222c4916647268c
-size 272
+oid sha256:ae47f3c2e17af366c5216d4d7ed8985a5b77c12d2735089242a16ba3cddfd52e
+size 269
