1a717a8e57309f5fc2256ffe742dee313abd4ef2
강지헌 <wlgjs0458@naver.com>
2024-05-08 11:05:05 +0900

    feat(be): 회원가입 유저 정보입력 api 추가
    Former-commit-id: 881de5302dfea3dbfbd0bba1ec132876b7b0f662

--- user-service/src/main/java/com/a508/userservice/user/domain/User.java
+++ user-service/src/main/java/com/a508/userservice/user/domain/User.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e20f1f144e613075c909e1471d608d574ad06f730d6a8adefb8a6c0aabe6d035
-size 1096
+oid sha256:d3c3c0eabb4a0a744e0a83878acc9da214398510fdcdd9be200d9e48524245ba
+size 1163
--- user-service/src/main/java/com/a508/userservice/user/service/UserService.java
+++ user-service/src/main/java/com/a508/userservice/user/service/UserService.java
@@ -1,7 +1,3 @@
-package com.a508.userservice.user.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserService {
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:6966915b1e45f2d2344d7dc5e60107cdc897a7aa7e7280350a84ff8545b3d01c
+size 1416
