be170645eb41c889f7608a09ed8672f39fd71aac
권정훈 <dhtmxk8134@naver.com>
2024-05-08 16:25:30 +0900

    feat(fe): 회원가입 기능 구현
    Former-commit-id: 96347dea499fd8b8e843edc42f00379b755385cb

--- client/src/app/(intro)/login/authentication/_components/Authentication.tsx
+++ client/src/app/(intro)/login/authentication/_components/Authentication.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5fc1899de8c1499560250c8f2411cd6a7fd509b83a27d018dee14ca55d2ca2a2
-size 1424
+oid sha256:ca0749de30d0eb2ed0af3726c6df70bde1957ddc451f60b1480384f33c79f3b7
+size 1429
