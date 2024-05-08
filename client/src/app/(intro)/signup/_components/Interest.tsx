008a743c8809164f2407cc57a2146fe47ba9b6ee
권정훈 <dhtmxk8134@naver.com>
2024-05-08 12:38:56 +0900

    feat(fe): 회원가입 기능 구현
    Former-commit-id: f5a6b5060eb0226fea785c2b3dedfa9c55d8db9d

--- client/src/app/(intro)/signup/_components/Interest.tsx
+++ client/src/app/(intro)/signup/_components/Interest.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37ec932fb5d9c2ea9b11b793cb9862fbcbb7bd936765cf078b26ffc776e8f77b
-size 6006
+oid sha256:2d05295c86c68c56842b8af199db2ab6693228e023acc0aae58af66b19c90cef
+size 6717
--- client/src/app/(intro)/signup/_components/Nickname.tsx
+++ client/src/app/(intro)/signup/_components/Nickname.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dff1e55cfe78ff852ec6ccf01b95e5f1cfcdab14bf6be54ffc8af74f119e94c1
-size 1856
+oid sha256:5261a7baad3d11aadb0cf3257f50174e1b33067e8bf8273c316b4c45b77dbdb4
+size 2335
