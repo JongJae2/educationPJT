9b28de300771b684da3a7aa909818e21aaf253b9
최정윤 <janet971015@naver.com>
2024-05-08 14:33:29 +0900

    refactor(be): cors 처리 수정

--- apigateway-service/src/main/java/com/a508/apigatewayservice/config/WebConfig.java
+++ apigateway-service/src/main/java/com/a508/apigatewayservice/config/WebConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba6ab5118e9ab558a924aa9bc76d21f0ea6dc58da099252f3dc5d931526916ea
-size 1185
+oid sha256:637c62144b2ad13603de045d986b03789e9c06d37d048d4106d8400a7787c0ed
+size 1435
