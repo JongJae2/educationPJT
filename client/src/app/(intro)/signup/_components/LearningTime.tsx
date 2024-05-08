371f34c2545c6f67c90cda65e9e9830fe2a8a4d6
하재률 <wofbf2@gmail.com>
2024-05-08 15:40:32 +0900

    design(fe): 회원가입-학습시간 UI
    Former-commit-id: d1cb86795f496a2caffef62b02a92ce20631a9b1

--- client/src/app/(intro)/signup/_components/LearningTime.tsx
+++ client/src/app/(intro)/signup/_components/LearningTime.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73f67f35bc9706cfe5288b5c32bb0eea6dcd61244a6f83d66e56981a83361062
-size 2085
+oid sha256:99d3cf45046420c648b8ef4bffda9c713d19106626055a66553f8cc9b26f07e8
+size 4212
