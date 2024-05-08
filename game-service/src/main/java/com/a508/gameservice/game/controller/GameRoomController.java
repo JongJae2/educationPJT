ddcca9b1c6cbbaff2d78ae8c9cb8124c616b5656
최정윤 <janet971015@naver.com>
2024-05-08 16:05:29 +0900

    feat(be): rankings api 구현
    Former-commit-id: 50036dd7dc7ec9df2a052572983beb4d66f4b04a

--- game-service/src/main/java/com/a508/gameservice/game/controller/GameRoomController.java
+++ game-service/src/main/java/com/a508/gameservice/game/controller/GameRoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:477e93081031af0c815fa7cf59e98c4b30acf318e301982492b2bed20e4799ff
-size 1071
+oid sha256:012ee94a7f08bb5d91aa0238a1fddfb8f199d7a87e7aeae387f9d892d6970b55
+size 1674
