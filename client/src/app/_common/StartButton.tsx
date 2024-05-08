e00bca0ce1107942950fa4182d829601685a2e5a
권정훈 <dhtmxk8134@naver.com>
2024-05-08 17:39:26 +0900

    feat(fe): 시작하기 경로 수정
    Former-commit-id: 889280db6b1a2ac691add0bf2a95c5aa1b5d3980

--- client/src/app/_common/StartButton.tsx
+++ client/src/app/_common/StartButton.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1217044b8c1ce08743b4bacd502883727fdcd6405c03fc79fcea340136f256cb
-size 378
+oid sha256:70e2892d77c8080c861ce08e9abc02488d34878a36fcebb888cf3463dee635d2
+size 379
