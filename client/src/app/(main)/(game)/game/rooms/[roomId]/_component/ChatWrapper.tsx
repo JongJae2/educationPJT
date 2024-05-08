9755aa55f15b53831fff0bab551949f7ef437a74
이지원 <dlwldnjs950@naver.com>
2024-05-08 14:16:53 +0900

    style(fe): 배경화면 서식 되돌림
    Former-commit-id: 6b75e84a2790cccfec636b04c0ee6ac43295d8a5

--- client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
@@ -1,3 +1,3 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1beaf0b5390d7e32eeafd5393bd73fbc821acd23f44d25abe8626f517baa0abb
-size 102
+export default function ChatWrapper() {
+  return <div className='bg-slate-400 w-3/4 h-52'>yo</div>;
+}
