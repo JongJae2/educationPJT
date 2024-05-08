6ccb8e57176dce27ed350962d1b21675e7f21da9
이지원 <dlwldnjs950@naver.com>
2024-05-08 15:18:05 +0900

    design(fe): 채팅 영역 메시지 추가시 스크롤
    Former-commit-id: 589dcb8d0d379fc4604d45ca2ed2039269e74692

--- client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
@@ -1,3 +1,3 @@
-export default function ChatWrapper() {
-  return <div className='bg-slate-400 w-3/4 h-52'>yo</div>;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:a55cda5da46e224f50f6f6858006a29e29a35f62ddbc11da554b38c719e074de
+size 1420
---
+++ client/src/app/(main)/(home)/home/page.tsx
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ab2eeb87c467ac003848ec2a810e2d8ea4bfe453c8ec74a736fa1bb61f0632c0
+size 360
