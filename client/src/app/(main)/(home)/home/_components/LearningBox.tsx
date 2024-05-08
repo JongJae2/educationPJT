8bba171c1a48ea9da991fbeb28616f6dbe82d3f9
권정훈 <dhtmxk8134@naver.com>
2024-05-08 19:59:27 +0900

    feat(fe): 메인페이지
    Former-commit-id: 2774b9f98b0aa6ef7d674093eaa4c70e9f9e6ab2

--- client/src/app/(main)/(home)/home/_components/LearningBox.tsx
+++ client/src/app/(main)/(home)/home/_components/LearningBox.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ee131b3fb4557373f82c477869fa4e5b44ea149d4fb5ecd9bca8fa89e0156f1b
-size 806
+oid sha256:29dc4e89de1b24a4ffadd0b83107cebada87fa21deba0dae639778914e2920dd
+size 1080
