7dacfadd4abaa7fb16cf84dee2c1c59d2ba7e78a
이지원 <dlwldnjs950@naver.com>
2024-05-09 16:57:16 +0900

    fix(fe): isLoading
    Former-commit-id: 935ec56ab77e75dd3fcade953f12d2bc5d453909

--- client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
+++ client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4e5309900f902c06e044f09238a63b38d1d90b9ae085e7c30cf7f9be8c216119
-size 2809
+oid sha256:7232120881e7e8e7c6f9c493daabde6d1d90bea8cdf0c1a061494bd419a6d37e
+size 2831
