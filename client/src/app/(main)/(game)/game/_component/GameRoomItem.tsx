3495cd7c8452e08db827a92fc8a96988e9eb0bed
이지원 <dlwldnjs950@naver.com>
2024-05-09 11:00:10 +0900

    design(fe): 게임중 보라색, 활성화 파랑, 비밀번호 회색
    Former-commit-id: e446ac66806cc4ee31409a2e97a94d4aac5a0889

--- client/src/app/(main)/(game)/game/_component/GameRoomItem.tsx
+++ client/src/app/(main)/(game)/game/_component/GameRoomItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fb3ea81184996ce9675dac5accd2a992e3e30d09053f1606a9da5f52e24d944a
-size 5339
+oid sha256:c49c709a333d50bf3639bab9cddd3f83ffcf877b524de5d89bef1cfe849d1c09
+size 5478
