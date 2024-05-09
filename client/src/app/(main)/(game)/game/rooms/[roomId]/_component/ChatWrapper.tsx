3b47546261dfd01547a06322d004b5fc5e9163fd
이지원 <dlwldnjs950@naver.com>
2024-05-09 18:04:33 +0900

    fix(fe): 웹소켓 https(wss) 요청
    Former-commit-id: 14f9cfb4b9a4ccf949f50e3ce59c6a8e3fb1bef8

--- client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4d62c1c23ecaabfb74ffdc43a96464dcc1c694f360b4cc1954341ea4ca836b11
-size 3004
+oid sha256:225dffd5e73d3cb625c821fc4fbf727c73b1990407a8f549218236e93b456e2e
+size 3005
