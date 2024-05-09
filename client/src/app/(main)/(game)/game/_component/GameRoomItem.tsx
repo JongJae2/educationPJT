29a973a3b13f56fd910021f34b47fd2c8375cb2b
이지원 <dlwldnjs950@naver.com>
2024-05-09 14:41:13 +0900

    feat(fe): 방 비밀번호 확인
    Former-commit-id: e777221c4758159d5f9376124b222e5dd5029da2

--- client/src/app/(main)/(game)/game/_component/GameRoomItem.tsx
+++ client/src/app/(main)/(game)/game/_component/GameRoomItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44bc9e0581fe008cbe33bdc5f2a71ec0b73bf60b31b84958b2845d73730b7628
-size 5774
+oid sha256:bd99c74fcca726c9f2cf0bd4e3c5fdb76b1aca14f64188c15bddab6c75ed0b37
+size 5615
--- client/src/stores/game-wait.ts
+++ client/src/stores/game-wait.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b0e2c5a54702df0add23668f0f6bbcf49fcbd2d12a5b940bfe34ec9cbdf0902
-size 1237
+oid sha256:15913acb2881c185612d7fdb59ee3ae06f848faf6727f6fb0d98c6f755763361
+size 1056
