135b96ceb488bf33063afd73722990d8f850aa2e
최정윤 <janet971015@naver.com>
2024-05-09 09:54:56 +0900

    refactor(be): NewRoomRes -> JoinRoomRes
    Former-commit-id: b226bb224fa7fe3e43c563842636ee8260150be8

--- game-service/src/main/java/com/a508/gameservice/game/service/GameRoomService.java
+++ game-service/src/main/java/com/a508/gameservice/game/service/GameRoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d575cc315051f3de2cd3bafd00298d81035c43a0b6b6a352a9f2460aedd15a0c
-size 8917
+oid sha256:a3d15da00f9602452b5cfae2485e41fd799ae05a0c890a04ee5665f8b5fb4501
+size 8956
