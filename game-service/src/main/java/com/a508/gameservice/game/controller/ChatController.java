54aede7b42510d6229f126512a887ed45237698d
최정윤 <janet971015@naver.com>
2024-05-09 18:38:44 +0900

    fix(be): stomp 주소 변경
    Former-commit-id: 132b725b04a1811b6691d8e169c1172a8a3d8b20

--- game-service/src/main/java/com/a508/gameservice/game/controller/ChatController.java
+++ game-service/src/main/java/com/a508/gameservice/game/controller/ChatController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3af13d9cbabf04fe3a4916a4efd05464f7be251376be32c79fbca36b1c3b51aa
-size 1673
+oid sha256:c184a8784a20636bf050f79cceaeb42ff79f53668b40e34cdcb2a733ef54743a
+size 1671
