10dd382473551b9f9c9725eb63e4d88cff0aa36a
이지원 <dlwldnjs950@naver.com>
2024-05-09 15:01:26 +0900

    fix(fe): merge conflict 해결
    Former-commit-id: 34b3f5f669502113c6ce450178648bf75b3f7836

--- client/src/app/(main)/(game)/game/layout.tsx
+++ client/src/app/(main)/(game)/game/layout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4fb4889044950a536db3715ebad36198a40c1b07b4761a2c08ffba91a003d9e
-size 1013
+oid sha256:956883ac431f7feeae84cc7d4d9dcc7a767412c31e6d5e05bab4537211261ba2
+size 1014
