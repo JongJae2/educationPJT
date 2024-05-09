c17e28562a1c8d100a3e55574dfa315a0844fa1a
이지원 <dlwldnjs950@naver.com>
2024-05-09 11:00:10 +0900

    design(fe): 게임중 보라색, 활성화 파랑, 비밀번호 회색
    Former-commit-id: 66a16dd2b10ddd50b9fbbcd75a2352ad1c325387

--- client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
+++ client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7362bd768cb9da789037f0bc56da0f7a331ddd47e4102939e76684820f2fd3e5
-size 3919
+oid sha256:f61df4037d7dbd6d9d84587147f3ea0a3af07f2c54d8c32a49148d560ffdf5ad
+size 4014
--- client/src/app/(main)/(game)/game/_component/PagingBtn.tsx
+++ client/src/app/(main)/(game)/game/_component/PagingBtn.tsx
@@ -1,7 +1,3 @@
-export default function PagingBtn() {
-  return (
-    <>
-      <div className='bg-ourDarkGray/80 cursor-pointer rounded-xl w-full h-11'>yo</div>
-    </>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:34c80b04b3217f5a5ae4901f732cb90225eda2352d749a3d6123b483be7fc550
+size 966
