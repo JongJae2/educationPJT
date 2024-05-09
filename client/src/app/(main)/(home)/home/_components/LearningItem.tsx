bc0f23ece6927f92a14ba5a4ec986af156bbb06d
권정훈 <dhtmxk8134@naver.com>
2024-05-09 13:28:48 +0900

    feat(fe): 객체 변수명 수정
    Former-commit-id: 4e9528f33b1fc7b70cc6d2072207467ee5f5f979

--- client/src/app/(main)/(home)/home/_components/LearningItem.tsx
+++ client/src/app/(main)/(home)/home/_components/LearningItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e841cea87a8025b2a2f501faadab703173e916a4adf108f46d7b24e5d451e46
-size 8659
+oid sha256:5273c0fddd8abf42f7874f77c4b8a737e2a3b82f9fabd84cd5cbac314eaf23d6
+size 8807
