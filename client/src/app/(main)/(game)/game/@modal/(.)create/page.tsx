9424a0fd07854b21d984eb53f29da1891c9d2e50
하재률 <wofbf2@gmail.com>
2024-05-09 16:04:57 +0900

    refactor(fe): 방생성 API 리액트쿼리 삭제
    Former-commit-id: 1e0e3ae113db2749ff40e9f8a41fe0aaef56df85

--- client/src/app/(main)/(game)/game/@modal/(.)create/page.tsx
+++ client/src/app/(main)/(game)/game/@modal/(.)create/page.tsx
@@ -1,3 +1,9 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3419ac4ef1ed628f5916741bd1bc92c3421a3b6551741684c066c470cab966d9
-size 157
+import CreateRoomModal from '../../_component/CreateRoomModal';
+
+export default function Page() {
+  return (
+    <>
+      <CreateRoomModal />
+    </>
+  );
+}
--- client/src/app/(main)/(game)/game/_component/CreateBtn.tsx
+++ client/src/app/(main)/(game)/game/_component/CreateBtn.tsx
@@ -1,3 +1,11 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7974f9ce8c70880aaf2f2c6188a04380492434de15c16f86a31917ce3391ab69
-size 177
+'use client';
+
+import Link from 'next/link';
+
+export default function CreateBtn() {
+  return (
+    <div>
+      <Link href={'/game/create'}>방만들기</Link>
+    </div>
+  );
+}
