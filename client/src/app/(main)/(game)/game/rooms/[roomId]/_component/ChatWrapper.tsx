40b98bda68ff0ca83dbff15b2beb84eaf289e589
이지원 <dlwldnjs950@naver.com>
2024-05-09 17:34:24 +0900

    feat(fe): 채팅 구현
    Former-commit-id: e2d5e9747b75ded093a241465a0550f86a6df4e1

--- client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:055a6badb916a7ade4dfc11ed3bd4a87c899d089e1d536c4dc755b780a13bc20
-size 1567
+oid sha256:c18fdf79cef402a335c880003eb6f2dee7ec10f2c2f2a9d8086fa3409a9dca01
+size 2936
