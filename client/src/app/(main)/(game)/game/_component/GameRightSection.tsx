ef5e07237e17c370dd7be5c2dfe0a39d818c9ebe
이지원 <dlwldnjs950@naver.com>
2024-05-09 12:46:38 +0900

    design(fe): 게임방 레이아웃
    Former-commit-id: 5c97f3410368f29ae0827243039be5ddb0d6156b

--- client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
+++ client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bed149c03f361fb75f8896208b1bd8b0b481ee2d3847fe2712b24ec67b69b9fb
+oid sha256:2a003147fe82071e33fa721ec1e8eeee85fa71c5d0abe67fdf1a035fcb3a5f40
 size 4036
