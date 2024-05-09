a2b9b25722c73cd5b2b04a839bb1205fab72684d
이지원 <dlwldnjs950@naver.com>
2024-05-09 13:20:39 +0900

    feat(fe): 게임방 요청
    Former-commit-id: 76602b7422379e29c6487fbf06bec8390355d796

--- client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
+++ client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a003147fe82071e33fa721ec1e8eeee85fa71c5d0abe67fdf1a035fcb3a5f40
-size 4036
+oid sha256:4714216f25976c3f6b64022795f43cb055893cb0c7127a733ffce4b401adf2d4
+size 2786
