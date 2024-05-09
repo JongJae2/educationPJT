8a6e9a15328c775850c75e9236504bc4db8204bd
이종재 <ljj452@naver.com>
2024-05-09 11:20:47 +0900

    feat(be): 신규 회원 오늘의 학습 생성 기능 추가
    Former-commit-id: 64361bba05df32779d331ab9ba89c605acd75b6a

--- study-service/src/main/java/com/a508/studyservice/service/TodayLearningServiceImpl.java
+++ study-service/src/main/java/com/a508/studyservice/service/TodayLearningServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8397d8e0578253d94d89bff2e6cbe39005e1e18c650228e4484af7b529e41342
-size 5602
+oid sha256:3656179ccb655ad553e5917dc02a04d772e255675c5bc5f12f4c1bd7ea7fe04b
+size 7637
