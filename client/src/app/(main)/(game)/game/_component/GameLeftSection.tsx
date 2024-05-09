f22ef87ea21b30a2156c60e8a8a8150977897eaa
하재률 <wofbf2@gmail.com>
2024-05-09 13:15:47 +0900

    refactor(fe): 랭킹 조회 API react query - prefetching 삭제
    Former-commit-id: 72696cf22da0527e26023a38570b10a48ad4b3da

--- client/src/app/(main)/(game)/game/_component/GameLeftSection.tsx
+++ client/src/app/(main)/(game)/game/_component/GameLeftSection.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f1916c25a7d254620e0cfc3c0a1c38936e28277e06537649c1f5f6bf25e02f74
-size 5447
+oid sha256:9e70560c6540f5ee167aeaab8ec86691449bda02a3c80f58893fd593c3c24247
+size 3981
---
+++ client/src/queries/get-ranking.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b57c6c17c9c2422b0bab08e5cdccc0abe7ffad4299ce65efde6f07fc6a6785be
+size 596
