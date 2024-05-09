276052fbec3ece4ca5ef25664fa0ea568a37d7ab
권정훈 <dhtmxk8134@naver.com>
2024-05-09 16:58:05 +0900

    feat(fe): 어휘 퀴즈 받아오기
    Former-commit-id: 1842ad2dfe3bce9711e3d83a3a6ff6af2df32c58

--- client/src/app/(main)/(learn)/learn/_components/Word.tsx
+++ client/src/app/(main)/(learn)/learn/_components/Word.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7c82f55fc2ae6b6adefe957c2ab308d023e9f36bcb9821cca34ddbc2c6408061
-size 553
+oid sha256:3e410150a7e1bb63efb1373f268ff7b0bb5f906a59a222011038a4939c57ee48
+size 596
