7215a9d2132bf954331b3949704fd22bb6bf230e
이종재 <ljj452@naver.com>
2024-05-09 09:26:09 +0900

    feat(be): 오늘의 학습 스케쥴링 기능 추가 중
    Former-commit-id: abf0728ef0ef71d43a58d5115c6648e3948e7811

--- study-service/src/main/java/com/a508/studyservice/service/SentenceInsertServiceImpl.java
+++ study-service/src/main/java/com/a508/studyservice/service/SentenceInsertServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:96119c1ef1652f11be620a006e4783ca0eee45775fd7cff49e3b84a4fb36f75b
-size 3645
+oid sha256:2a692c07583438e2ed635533905cc1ff81cedccf49a0f40b95517ab7beb0d03a
+size 3887
