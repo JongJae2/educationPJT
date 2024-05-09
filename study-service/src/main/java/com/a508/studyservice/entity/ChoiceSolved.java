56f9bab05d00ca84b99d63bf0823266524b9fca2
이종재 <ljj452@naver.com>
2024-05-09 09:27:30 +0900

    feat(be): 테이블 컬럼 추가
    Former-commit-id: 8b0f77466a60dcdd63378d09b27fbfed1ce690c2

--- study-service/src/main/java/com/a508/studyservice/entity/ChoiceSolved.java
+++ study-service/src/main/java/com/a508/studyservice/entity/ChoiceSolved.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de7c562e66d88bd6da2978b6d530ddac910f098eee206cb065a3a608bb6e90f8
-size 820
+oid sha256:b16a1fc64d522e3eb381d34942b98026318685a6a506aeacce277d9c9ee34b55
+size 852
