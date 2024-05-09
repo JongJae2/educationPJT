41b010225d0b04958515b5ed696fad916482f245
이지원 <dlwldnjs950@naver.com>
2024-05-09 14:58:22 +0900

    style(fe): 게임 배경 바꿈
    Former-commit-id: 6cba203e61a957f6c12e37d49a44dc0570d5bc32

--- client/src/app/(main)/(game)/game/layout.tsx
+++ client/src/app/(main)/(game)/game/layout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b0cda1dc70e36baa273ac2322b04136ea1049d7d817866fb3511858488bdac6e
-size 1007
+oid sha256:e4fb4889044950a536db3715ebad36198a40c1b07b4761a2c08ffba91a003d9e
+size 1013
