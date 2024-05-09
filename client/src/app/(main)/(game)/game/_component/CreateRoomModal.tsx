2fdc6cf63de62ecf34d50181d7a0bb6a3b79779e
하재률 <wofbf2@gmail.com>
2024-05-09 16:20:47 +0900

    refactor(fe): 방생성 모달 path 재설정
    Former-commit-id: 0d2f283de1dc72931e80b226f3b9fb60c0a5ee2d

--- client/src/app/(main)/(game)/game/_component/CreateRoomModal.tsx
+++ client/src/app/(main)/(game)/game/_component/CreateRoomModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e8ecd24e71af63a99fc831e682b8e49e51b4ae7b6afafbdf1893c41d17f01086
-size 8647
+oid sha256:e5e4287437682a92cec8e9ae0cbaeef5bb17bf4e52535dadecf7015968a8de22
+size 8648
