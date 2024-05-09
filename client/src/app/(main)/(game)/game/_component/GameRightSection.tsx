33bb882713ba8396d1183033cd5567a89a3ccf56
이지원 <dlwldnjs950@naver.com>
2024-05-09 14:56:08 +0900

    style(fe): 배경색 제거
    Former-commit-id: 4818039d302e037a9902fa5e533d527ca8282c1c

--- client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
+++ client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9002866773079b75e8962c98d2f41eeed0acb7a85e436d6807cb579aac4e27f
-size 3955
+oid sha256:4714216f25976c3f6b64022795f43cb055893cb0c7127a733ffce4b401adf2d4
+size 2786
