d52fa8dba500042df1c1c4e70aa8b635e0f1dbaf
이종재 <ljj452@naver.com>
2024-05-09 11:22:14 +0900

    fix(be): 오류방지 수정
    Former-commit-id: 2839b158639857a3b01a4e04089811dffa6203a3

--- study-service/src/main/java/com/a508/studyservice/dto/response/monthResponse.java
+++ study-service/src/main/java/com/a508/studyservice/dto/response/monthResponse.java
@@ -1,14 +1,3 @@
-package com.a508.studyservice.dto.response;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class monthResponse {
-
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:14cb13563411e5e01f910e79e4d640696685fa4bff7a5b7f620ae881f7712df5
+size 310
