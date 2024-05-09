9efe00b2eeb33fa772cdcde3d7ceeeeccc8aeb55
이종재 <ljj452@naver.com>
2024-05-09 09:28:00 +0900

    fix(be): dto 변수 추가
    Former-commit-id: a0dd340ae5cecc05291aaae567abdb3d0fc36789

--- study-service/src/main/java/com/a508/studyservice/dto/request/ChoiceRequest.java
+++ study-service/src/main/java/com/a508/studyservice/dto/request/ChoiceRequest.java
@@ -1,15 +1,3 @@
-package com.a508.studyservice.dto.request;
-
-import lombok.Data;
-
-import java.util.List;
-
-@Data
-public class ChoiceRequest {
-    String type;
-
-    List<Integer> myAnswer;
-
-    List<Integer> problemId;
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:b064c493e2c64c0fabc879d86766632294d12e0bbaeda0e5725d3053292018c0
+size 227
