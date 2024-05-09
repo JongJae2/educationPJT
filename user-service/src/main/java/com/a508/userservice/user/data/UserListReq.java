263d1749110d15bd8f55aa3c691c800e77419b8b
최정윤 <janet971015@naver.com>
2024-05-09 09:33:02 +0900

    feat(be): feign 통신 data 클래스 생성
    Former-commit-id: 88f1d2b6fb1d702c62d297bc37fddf04624926d8

---
+++ user-service/src/main/java/com/a508/userservice/user/data/UserListReq.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:869b2b5007b7680e8f79a85509aff8a17553093e592db0d0c0fd38d068b9ff11
+size 298
---
+++ user-service/src/main/java/com/a508/userservice/user/data/UserListRes.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:49d6e2945cf3b43e32797c0222af2dfb80d5e3d8a9a2479ed2aa843989cfb9d9
+size 295
