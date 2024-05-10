60bc4aabd00089af4a704fe846ec13f9d546c77e
이종재 <ljj452@naver.com>
2024-05-10 09:37:18 +0900

    style(be): 불필요 import 제거
    Former-commit-id: 0f7a89a2ef876244f873300293a6cf3a95f308c6

--- study-service/src/main/java/com/a508/studyservice/dto/response/ProblemResponse.java
+++ study-service/src/main/java/com/a508/studyservice/dto/response/ProblemResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba92c3c9f5ba1af415659a9a177947ce4881c7a6b388d9170e7e524620e8397c
-size 575
+oid sha256:cdff1b1da31ca3a5c4c92b698170a6bd12f41a699b1eac336d2c1e8bf6187209
+size 542
