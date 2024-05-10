aab2559e2e6a91a9aefc851781b814f9d1ff933c
이지원 <dlwldnjs950@naver.com>
2024-05-10 16:12:38 +0900

    fix(fe): 비밀번호 입력시에도 router push, 다음버튼 임시 활성화
    Former-commit-id: ad3125f3c017b0370cb00461b263d07cbe0d0ada

--- client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
+++ client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7232120881e7e8e7c6f9c493daabde6d1d90bea8cdf0c1a061494bd419a6d37e
-size 2831
+oid sha256:6b628a3ee2d8e682c4466d3c3ed369d9789e9b7734032a07f7b5e30f7872f6c6
+size 2892
