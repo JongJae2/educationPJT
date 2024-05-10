d4393519b78d6247a8ac4fd166f38cd774306448
이지원 <dlwldnjs950@naver.com>
2024-05-10 14:30:11 +0900

    fix(fe): 오류 해결
    Former-commit-id: 43a670b344af823129b6cfcf36923f9eb9668768

--- client/src/app/(main)/(game)/game/rooms/[roomId]/_component/GameScoreBoard.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/_component/GameScoreBoard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c61aa13e4f8a13b1c004f2e8848e0efceb0135ca5a4f7fd0edd50e51f3bbf2c5
-size 639
+oid sha256:ae6bc3c7a4baaeed1e2736dc0879efbe52bfdf46b194075c1d52da50072c7c00
+size 500
