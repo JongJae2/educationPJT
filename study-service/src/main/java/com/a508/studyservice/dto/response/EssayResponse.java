60a7c0c56d4e7b99309abfe254810bc922571589
이종재 <ljj452@naver.com>
2024-05-10 10:15:42 +0900

    fix(be): Dto 수정
    Former-commit-id: 817f3171b1afe699adf01318dd975b2d4e40e5b2

--- study-service/src/main/java/com/a508/studyservice/dto/response/EssayResponse.java
+++ study-service/src/main/java/com/a508/studyservice/dto/response/EssayResponse.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6915102e3e8f8a73964d9b0a465ea3f6aa4e7c314b56b36c48dd48d10f3a646d
-size 528
+oid sha256:340013956bc96d9fe6d8df95808503b48f5c2a80695295816db736ce060d572e
+size 491
--- study-service/src/main/java/com/a508/studyservice/service/EssayServiceImpl.java
+++ study-service/src/main/java/com/a508/studyservice/service/EssayServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d74bdd608a894917dbcf5d82a4be26377992662a3baaa924c96502fe67efda1f
-size 4795
+oid sha256:be5d9d2c374d2e2af350ca5bd88b2532c7a06a1ff7709bb672382ebce301ed84
+size 4728
