37389b1bd3347b431f312bc36c5592adb092e7d5
강지헌 <wlgjs0458@naver.com>
2024-05-10 09:16:04 +0900

    fix(be): fasttext import 수정
    Former-commit-id: 45a4cb6ce06bb80469a277a95ba8488b5ba73e2d

--- recommend/recommend/app.py
+++ recommend/recommend/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d704f8c6aaf3fbfe755c7afab828fb43a449cf58d69aec80def9b4224333ba1e
-size 4326
+oid sha256:54ef510acb59e533e436efa2be838c0025e10292812b52b45840face9877e4cc
+size 4289
