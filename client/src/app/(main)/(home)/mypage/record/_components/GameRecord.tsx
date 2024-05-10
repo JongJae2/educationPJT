51db954ed70d99d07ec6abf2b761e0073387c0a8
이지원 <dlwldnjs950@naver.com>
2024-05-10 17:45:02 +0900

    fix(fe): undefined 츨력 수정 시도
    Former-commit-id: 38750034a04d4740197308c648f05328240efe4d

--- client/src/app/(main)/(home)/mypage/record/_components/GameRecord.tsx
+++ client/src/app/(main)/(home)/mypage/record/_components/GameRecord.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc61ecad1e1ded30351f6e9dbaaf2443b4ee69f8a984df0cf7020bee6aee01c1
-size 3549
+oid sha256:ff48a748dc6437f14591fe0fedcd4f14271f2ed3138669b73035ddb37bee0265
+size 3592
