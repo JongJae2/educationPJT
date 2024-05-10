2984320788cf36f4de520c632924fd5e083a454d
이지원 <dlwldnjs950@naver.com>
2024-05-10 15:02:00 +0900

    chore(fe): console.log
    Former-commit-id: dd927fc0ea2478b87adc51c3cebf990b14ffa777

--- client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f42465cb6b07ded1ba4541153c590440da5c6627e878639ba172e17dde7b2908
-size 3277
+oid sha256:206b4046cb22cae393651d8f882493a3107f2dd45f1bbd568ec91a5f07a95fff
+size 3336
