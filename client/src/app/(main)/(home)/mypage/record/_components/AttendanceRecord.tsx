6fb441dd5584fe8a12267fd52763c7405b9c4512
이지원 <dlwldnjs950@naver.com>
2024-05-10 10:24:31 +0900

    feat(fe): 응답 데이터로 학습 영역 출력
    Former-commit-id: 7bb5b0af0139e3daa360ebb78cdc90f32468cafe

--- client/src/app/(main)/(home)/mypage/record/_components/AttendanceRecord.tsx
+++ client/src/app/(main)/(home)/mypage/record/_components/AttendanceRecord.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:597b48f70b33427a9e36e7281b48230ebf2eca4a2debc45203dcead7d9d195d7
-size 3731
+oid sha256:349c1f82a24e8aa46294fdee2900e18f94fd48a19ccf5d68c41db548aca8858c
+size 3778
