4f0d7770718843ba286320fadf5f19415a697b5d
littlebird <dlwldnjs950@naver.com>
2024-05-12 01:32:01 +0900

    refactor(fe): webSocketStore 사용 코드, 페이지 이동 전 connect
    Former-commit-id: fbddab1c91373616868a39aac63835910f2af630

--- client/src/app/(main)/(game)/game/_component/GameRoomItem.tsx
+++ client/src/app/(main)/(game)/game/_component/GameRoomItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20768e029baff80cd8134257a71d1d9145d90c1468e47a11b5fa93314dba90a2
-size 6547
+oid sha256:7074cafab0b5d4a196e02ad0b5cbfc14d5eda0c9f124ee21d1e537ae95ed8fd2
+size 6683
