0a1fbe3e71efa0b5ee943d814bab8a9ffc54cde8
littlebird <dlwldnjs950@naver.com>
2024-05-11 22:37:22 +0900

    fix(fe): 응답 데이터 형식 수정
    Former-commit-id: 6b24d8c21c4998853c7b3fde35ae03773dae62a2

--- client/src/queries/mypage-record.ts
+++ client/src/queries/mypage-record.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4b5acaa54acd1e449a8a4468cea8162b6dc5bfdfe48cbf0404f5a96478c16e6b
-size 2146
+oid sha256:4233781e11ec9c4ee8e77561cf636db891749d9ef3f4d810fe231682dee85bf6
+size 2156
