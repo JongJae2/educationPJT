b6c4d1f240c60fa276283a9a209fd5eed45738e3
littlebird <dlwldnjs950@naver.com>
2024-05-11 22:42:20 +0900

    merge(fe): fix merge conflict
    Former-commit-id: ae2ab4a867475078489b5281da3c530a2a1de46a

--- client/src/app/(main)/(home)/mypage/record/_components/GameRecord.tsx
+++ client/src/app/(main)/(home)/mypage/record/_components/GameRecord.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4f6d1781d673574f81487ec20e4e84a45baf387cbc5a5baf8e306acda3c71a66
-size 3486
+oid sha256:43560efe9c28169091f573101b447f57302d93009c8cd3dc70cb9cb035233eee
+size 3491
