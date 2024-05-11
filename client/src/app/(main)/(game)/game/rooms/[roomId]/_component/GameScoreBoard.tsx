b98949e2354b99969efa68f0eef6c1d11f2e64eb
littlebird <dlwldnjs950@naver.com>
2024-05-11 21:38:28 +0900

    fix(fe): merge conflict
    Former-commit-id: a51abfd2cc346521717cfe2ed9dc235e3c9dfe58

--- client/src/app/(main)/(game)/game/rooms/[roomId]/_component/GameScoreBoard.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/_component/GameScoreBoard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:172eee4aaf434a85368c297e71e1c4d876e97996da6a6c85ad505d0769a1c28a
-size 1267
+oid sha256:515cd8ebd83f85ebff923a131a6a7405fa3ed55c7e1ba5f3b15a112687071ee8
+size 1269
