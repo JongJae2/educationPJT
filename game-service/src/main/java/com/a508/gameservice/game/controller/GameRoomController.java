736862ef1f337bd0bb6c5a139d2f96ccc9cf78d6
최정윤 <janet971015@naver.com>
2024-05-12 00:40:25 +0900

    feat(be): 방 생성할 때 Scheduler도 추가
    Former-commit-id: cd9e676443e0275c82a9909c2dcd3fa8103d488f

--- game-service/src/main/java/com/a508/gameservice/game/controller/GameRoomController.java
+++ game-service/src/main/java/com/a508/gameservice/game/controller/GameRoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2a09e458809741009ab0e5007d00d2380359562a0d91a8d970dfc348483b6dec
-size 1411
+oid sha256:fef97aa0a3367969cac6fe5cec9b17f76ca7cf29e4ae50acf937283db7b20f2f
+size 1667
