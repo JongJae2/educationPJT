f19f67b5b1cc9c8c1f173932823a05480c9bf260
littlebird <dlwldnjs950@naver.com>
2024-05-11 22:37:22 +0900

    fix(fe): 응답 데이터 형식 수정
    Former-commit-id: 321a6d5163ed61f8b11a7951f996662a7ff5db5f

--- client/src/app/(main)/(home)/mypage/record/_components/GameRecord.tsx
+++ client/src/app/(main)/(home)/mypage/record/_components/GameRecord.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:50335c645bcf288fd7bf34caec98bb93f4914c994025aefe30dbfc411541e0d8
-size 3443
+oid sha256:3f0d52a8929cab7760f2d90c83db760c1bc5abb2462ffb85c5c94eebd7f59e69
+size 3464
