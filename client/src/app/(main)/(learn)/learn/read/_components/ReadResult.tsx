7e92b71447f5a355afb4e75c7bb7789fe38503b4
권정훈 <righthun.kwon@gmail.com>
2024-05-12 15:12:25 +0900

    fix(fe): 정독훈련 타입 수정
    Former-commit-id: 9bff702a95595d081a5af3ed2c22a477551b7eda

--- client/src/app/(main)/(learn)/learn/read/_components/ReadResult.tsx
+++ client/src/app/(main)/(learn)/learn/read/_components/ReadResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6e07548a4da122c66c02e73dbb33a0d0d04da7a3d004f5b9940f42ce37f2df83
-size 7314
+oid sha256:57e48049c4b148984179ae06732b6f2607f018d98cb8ae71b759ff92dd81cd53
+size 5148
