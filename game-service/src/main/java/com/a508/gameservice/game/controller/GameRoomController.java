4195a482848698f33dcb0ca95359f541cb86c36a
최정윤 <janet971015@naver.com>
2024-05-12 10:13:40 +0900

    refactor(be): Page 반환 타입 변경
    Former-commit-id: dae3ed5bc36bbedd7b7b37aa96995928e2f9bbbe

--- game-service/src/main/java/com/a508/gameservice/game/controller/GameRoomController.java
+++ game-service/src/main/java/com/a508/gameservice/game/controller/GameRoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fef97aa0a3367969cac6fe5cec9b17f76ca7cf29e4ae50acf937283db7b20f2f
-size 1667
+oid sha256:ea5daf847d088a34ad355e99f28419f71063d2ce277131b60c0e0fcb24df40f9
+size 1644
