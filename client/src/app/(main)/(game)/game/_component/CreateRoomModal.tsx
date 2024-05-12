da6e0252b207a3c12c8a2e55f7e7b5ce45c0944b
하재률 <wofbf2@gmail.com>
2024-05-12 17:13:49 +0900

    refactor(fe): 방생성시 정보
    Former-commit-id: 24c7a5e70cb5ba4097e348b680c0528ef7013625

--- client/src/app/(main)/(game)/game/_component/CreateRoomModal.tsx
+++ client/src/app/(main)/(game)/game/_component/CreateRoomModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:94e1ce21b4bd95b368d42d34e78580f9396418486f61edc80accd433fc76cab1
-size 8540
+oid sha256:b2239eab65fb584b591d471b4c86cbb520e8deb69fd738460eb3207a1986fee1
+size 9031
