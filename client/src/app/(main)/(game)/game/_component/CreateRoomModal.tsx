f718665fbdc39e6e6b187da1b5b3eb82cb9c27d8
하재률 <wofbf2@gmail.com>
2024-05-12 17:49:31 +0900

    fix(fe): svg 폴더 수정
    Former-commit-id: 978420a488cd29e9af88778ec78bea2b5b0d46a1

--- client/src/app/(main)/(game)/game/_component/CreateRoomModal.tsx
+++ client/src/app/(main)/(game)/game/_component/CreateRoomModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0e85ccce233cb19bced99bbcd68336b2c3d00bfc1b2940d7dbd8f4148d2f8af
-size 8569
+oid sha256:ecd3f4dfdcc8ff317d3ccb2a6dc61c8f33e6376ce815f9ec6364d3211d8af2cb
+size 9070
