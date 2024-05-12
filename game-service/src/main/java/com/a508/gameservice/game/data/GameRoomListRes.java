d30055a7fa65f9ee691f8ca57606c5852903c3eb
최정윤 <janet971015@naver.com>
2024-05-12 10:14:55 +0900

    feat(be): 페이지 반환 타입 생성
    Former-commit-id: ad72c27cec9efb5ac9e72e020f31bc7d3b085f81

---
+++ game-service/src/main/java/com/a508/gameservice/game/data/GameRoomListRes.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:168b08330ab640b1a0f21bcfe8431872c13dc900ea3df79017a9c94bb79dacae
+size 239
---
+++ game-service/src/main/java/com/a508/gameservice/game/data/PageRoomListDTO.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:811b2824e5ffbedce36f819e590cfebe0a8cec15c2c71800b4e9190eb58d1fcc
+size 284
