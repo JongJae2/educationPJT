542316c79c7297201786a39a93db082bd8c3b3de
권정훈 <righthun.kwon@gmail.com>
2024-05-12 16:19:23 +0900

    fix(fe): 타입 수정
    Former-commit-id: 4b5994bb72af7dc18917f9d83b3ac808708f10cb

--- client/src/types/Problem.ts
+++ client/src/types/Problem.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:26fe3f9af2f362209869df6259b1d621db854dfac8fc261cc43e564e17687848
-size 724
+oid sha256:47a3274f0871f839c587bed398dbd9cd8c84bb8b3b99c73bc1a8d45b8efc7da6
+size 962
