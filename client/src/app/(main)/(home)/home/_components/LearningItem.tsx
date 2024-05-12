49fe9edbebe7dc0d6b1072df29ffc1eee9af286b
권정훈 <righthun.kwon@gmail.com>
2024-05-12 14:59:56 +0900

    chore(fe): 타입 수정
    Former-commit-id: af2e219dcef9f3cc53b1fdbce5be2ea167dbcbd7

--- client/src/app/(main)/(home)/home/_components/LearningItem.tsx
+++ client/src/app/(main)/(home)/home/_components/LearningItem.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec11846f064069796c8a4f71600add76014aa2b7a9008b380b4d1790014c19ab
-size 10443
+oid sha256:6ddf7cb77eb72280655cc7cc9b23a80b9d9d896951c2ec148e92d078dd3a68f5
+size 10592
