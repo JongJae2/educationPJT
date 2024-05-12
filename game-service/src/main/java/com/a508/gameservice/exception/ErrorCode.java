dec5f797554cf85276f9be534ede89fd692a4014
최정윤 <janet971015@naver.com>
2024-05-12 09:07:40 +0900

    feat(be): ErrorCode 추가
    Former-commit-id: 9546cf5abe35af04e85411e5777c22b12b360c51

--- game-service/src/main/java/com/a508/gameservice/exception/ErrorCode.java
+++ game-service/src/main/java/com/a508/gameservice/exception/ErrorCode.java
@@ -1,20 +1,3 @@
-package com.a508.gameservice.exception;
-
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
-public enum ErrorCode {
-
-    //Game
-    PLAYER_IS_FULL_ERROR(404, "GME001", "제한 인원을 초과했습니다.");
-
-
-
-    private final int status;
-
-    private final String code;
-
-    private final String message;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:24247df3c312ca63e1c2c13e747ae88785a4b34a3fa1a46dfaa61cfde7de5624
+size 500
