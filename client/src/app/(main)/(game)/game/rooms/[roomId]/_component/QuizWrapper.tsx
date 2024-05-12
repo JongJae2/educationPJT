af590dfd783a26c07ba68644093b6cc56f7476e6
하재률 <wofbf2@gmail.com>
2024-05-12 17:14:20 +0900

    feat(fe): 웹소켓 스토어 생성, 문제 및 타이머 구현
    Former-commit-id: eb312908753526e63999f6cd19e977e611e03237

--- client/src/app/(main)/(game)/game/rooms/[roomId]/_component/QuizWrapper.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/_component/QuizWrapper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75f4e722601d5da3ce61b439103d6a8cebe3c9c3b8fe7676d6a828ef2ccb852f
-size 1123
+oid sha256:762dea6c67b5674976b5bdc69a194ee5e6ee52c16a3b3bc6db43f280add469b3
+size 3063
--- client/src/app/(main)/(game)/game/rooms/[roomId]/_component/TimerWrapper.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/_component/TimerWrapper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8170d4dffde1e0c82bbc10d18c56a564f31f7e34d3a0aebfa78da0107c8182ab
-size 900
+oid sha256:48bdebc229743e9cece338efbbfa09a4b77138932225327df67b667ce76b4790
+size 498
