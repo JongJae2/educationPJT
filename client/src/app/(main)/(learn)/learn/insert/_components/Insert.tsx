209b1b9d2494ca7c90bbc1cdd4bbad3488529647
권정훈 <righthun.kwon@gmail.com>
2024-05-12 15:49:49 +0900

    feat(fe): 문장넣기 구현 완료
    Former-commit-id: d942173fd1d1bbd9d719ee38e6d334a938191f43

--- client/src/app/(main)/(learn)/learn/insert/_components/Insert.tsx
+++ client/src/app/(main)/(learn)/learn/insert/_components/Insert.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:531c8c7d78430a8a399182af60ac0dfbead77d65b9edce0c388c36d559672184
-size 7764
+oid sha256:b70a86cd2480fc620698ba9246672b610634336e5f5c330bf6004f85a10b00a5
+size 7609
