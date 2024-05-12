cf61fa8442407346562cd8a409c94668348376d5
최정윤 <janet971015@naver.com>
2024-05-12 16:31:24 +0900

    refactor(be): 입장 메세지 수정
    Former-commit-id: 7ce982a05ee7251a6da9b5559f93e7b192b687b8

--- game-service/src/main/java/com/a508/gameservice/game/controller/ChatController.java
+++ game-service/src/main/java/com/a508/gameservice/game/controller/ChatController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e03433ee72c90e6dba2cdedc8cf7dde2b3bce52284b83d35ad75070a45d5b0f1
-size 1673
+oid sha256:b6bc29198002d7e83cd702b7ea9cae271788974ab8f70b249042d443ebb554d2
+size 1639
