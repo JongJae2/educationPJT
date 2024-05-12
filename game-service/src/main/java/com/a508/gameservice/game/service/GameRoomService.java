593909dfd6b1bb3bdb7c9eca94fc72ef11cab4ff
최정윤 <janet971015@naver.com>
2024-05-12 10:09:16 +0900

    refactor(be): redis 페이지 구현 변경
    Former-commit-id: a55c4724fdaea7222d22c817dcac6ef5109e1ec0

--- game-service/src/main/java/com/a508/gameservice/game/service/GameRoomService.java
+++ game-service/src/main/java/com/a508/gameservice/game/service/GameRoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2146a3958bb78033f6a7267ed28812e5ebc04d41258cd6c997a22e137505fab7
-size 9070
+oid sha256:090a53e646e4265fba6bcb23520115c4ca6076498c723b62ad96cbef22c8e6a1
+size 8864
