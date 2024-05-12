a213c8c369c542d172c6ecbc999daaa3ce673007
littlebird <dlwldnjs950@naver.com>
2024-05-12 21:37:56 +0900

    feat(fe): disconnect시 퇴장 메세지 발행, 출력 메세지 색상
    Former-commit-id: c5ff88cbb1ffc09f12bd1314ffc417dc052ac98a

--- client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01540f2c82347c65486af5a2120898f270b2be13a77b2fbfac9274af9c0efa6a
-size 3068
+oid sha256:0dac09e31efcf6669a4f8b00c6b9a495adfc4da9d46278c2baab8fe4f6a30c26
+size 3280
