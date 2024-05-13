dad2f60a168e8c7315b09889486857943366a6be
이종재 <ljj452@naver.com>
2024-05-13 14:37:34 +0900

    fix(be): 토큰 로직 추가
    Former-commit-id: 8bc1dfbb71a2ae81e676e0d4eabfab6270722ef9

--- study-service/src/main/java/com/a508/studyservice/service/TodayLearningServiceImpl.java
+++ study-service/src/main/java/com/a508/studyservice/service/TodayLearningServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54971beef9b13a259f4a091fa22393f0d61b458ffcae9aabe7484c836d7ffce6
-size 8451
+oid sha256:847e6d530155e20b5d5818535737955473a5315408f91e3bc28c5ddd204341c0
+size 8236
