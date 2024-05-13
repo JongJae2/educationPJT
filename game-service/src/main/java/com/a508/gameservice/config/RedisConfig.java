a6dd4e25e233e2524f70b9cf6e056f21113d550a
최정윤 <janet971015@naver.com>
2024-05-13 09:18:44 +0900

    feat(be): RoomPlayerTemplate 추가
    Former-commit-id: 6cb139f30169c6c31aee7fb5e5838875d68847d3

--- game-service/src/main/java/com/a508/gameservice/config/RedisConfig.java
+++ game-service/src/main/java/com/a508/gameservice/config/RedisConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d91357ea1359045b917af8744baf1900834135f3e352cc8343a7dac2619f4008
-size 2538
+oid sha256:70e7edf5ad78da7f8ab6f061fe097483bac7e59234f92cf9181025b728e48cbb
+size 3448
