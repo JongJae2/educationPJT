ff03bccb95de4262c45453167f57f5522eb4198e
최정윤 <janet971015@naver.com>
2024-05-13 09:21:25 +0900

    refactor(be): 페이징 redis 방식으로 수정
    Former-commit-id: bbe0928720a4668b27d4e0447ffbbcc40e8ad42b

--- game-service/src/main/java/com/a508/gameservice/game/service/GameRoomService.java
+++ game-service/src/main/java/com/a508/gameservice/game/service/GameRoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:090a53e646e4265fba6bcb23520115c4ca6076498c723b62ad96cbef22c8e6a1
-size 8864
+oid sha256:566041603aee9cd714e19911871d31b6b1b5bce9c4fd64902222eef2f5b7768c
+size 9374
