49b8bcd57294c38c7eccaa020338f1cb2d03842c
이종재 <ljj452@naver.com>
2024-05-13 15:39:25 +0900

    style(be): 불필요 코드 제거
    Former-commit-id: fa1a6bc7f5358d0ce38ffbbe8faf939bd5acb56f

--- study-service/src/main/java/com/a508/studyservice/service/ChoiceSolvedServiceImpl.java
+++ study-service/src/main/java/com/a508/studyservice/service/ChoiceSolvedServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6feb6796ce4e429cb9243a4d5398e392f1614559d68f3c71e4471da69ca14c43
-size 6823
+oid sha256:4315adbe501e38568ec20408e1bec4a092f559d360cfc56a304b015dd78f5296
+size 6822
