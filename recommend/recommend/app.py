301bd6da3e970259bbad86efbbb725587e06587d
강지헌 <wlgjs0458@naver.com>
2024-05-13 13:56:16 +0900

    fix(be): json 형식으로 변경
    Former-commit-id: bb9042a4cc8b7bbde8b7201ca35847bb625fd76a

--- recommend/recommend/app.py
+++ recommend/recommend/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ba5609bc7602a56339f350cdd2d46b17b1febeca3935bf138147527f2682808
-size 4576
+oid sha256:6b1c15724f8c1b5ab834bf73b4b4d9ac62f30d43a2a48a46b3c9d465958073c9
+size 4623
