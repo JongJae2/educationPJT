8e44ee790c352f9246d295b85cb8ba4b10ca7221
이종재 <ljj452@naver.com>
2024-05-13 17:40:53 +0900

    fix(be): 토큰 로직 오류 수정
    Former-commit-id: 51f34b42309a297d1adc02bb5f80ea1df099e92a

--- study-service/src/main/java/com/a508/studyservice/service/FiveAbilityServiceImpl.java
+++ study-service/src/main/java/com/a508/studyservice/service/FiveAbilityServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:82fb1a68c5fbae272ae6b98ff7902c01649b673d9e6edb2e12e5bcadf7f74265
-size 1417
+oid sha256:afdd5251c0c4b9452800c61579c59df9ac3a3c2a1dd28514fe9c26637c7f1f85
+size 1471
