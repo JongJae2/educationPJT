cef1ddc78bff332f03276f1c373d2a9cfc33d4f3
하재률 <wofbf2@gmail.com>
2024-05-13 19:57:47 +0900

    fix(fe): 첫문제 바로 답, 정답 한 명만 맞추기, reconnect시 정보 불러오기
    Former-commit-id: ea6dc0554f1cbfe31dfaefb750b94df1bb8eedf0

--- client/src/app/(main)/(game)/game/rooms/[roomId]/page.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c4af6630689ff7b8251f50eceddadbe58e82d4a3fd9635bf9407809918c7eaff
-size 1969
+oid sha256:be0ce5b8eb80f392a4ce50421e3aea0bd49608f68c5def408a4f09a2935ba933
+size 2415
--- client/src/stores/websocketStore.ts
+++ client/src/stores/websocketStore.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:054870fedf71d30eb963ece9d72c9cffd25984a601b1456baaf49aa05921e4d5
-size 4803
+oid sha256:b11433772ed2d1a9064afb1b1472a92936825156dca3a5e5a738c50884767e36
+size 4710
