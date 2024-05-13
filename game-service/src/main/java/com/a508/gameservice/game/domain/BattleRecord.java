c701ab7d8643466e2ab9db3b8490007b58d9a667
최정윤 <janet971015@naver.com>
2024-05-13 15:28:02 +0900

    feat(be): BattleRecord 변경 메소드 추가
    Former-commit-id: 780b727ebbdfcdcfcace2607cc8c825120e3d7ad

--- game-service/src/main/java/com/a508/gameservice/game/domain/BattleRecord.java
+++ game-service/src/main/java/com/a508/gameservice/game/domain/BattleRecord.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e0b7c031b1ce2002ac993f6b0f49d3bc39b34dd58393714621e402c0d0ad6a68
-size 589
+oid sha256:7830bc9a9c1561c6db723e0b3ee9627e91a2fcc44afb8175c2d8b83df187beba
+size 802
