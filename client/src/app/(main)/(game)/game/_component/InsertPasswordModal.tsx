d87c388c0566d711b8e6b359a8231ce7a12d55ad
이지원 <dlwldnjs950@naver.com>
2024-05-13 11:02:28 +0900

    fix(fe): 방입장 훅 이름 바꿈
    Former-commit-id: 73e5f40da0d218b4478389bbdd6e6e9bd6254b46

--- client/src/app/(main)/(game)/game/_component/InsertPasswordModal.tsx
+++ client/src/app/(main)/(game)/game/_component/InsertPasswordModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b6883703e7980f60b9a83fa346751ef3e6638acc3a007540d6056583d4d7abd0
-size 4138
+oid sha256:a39e3441f6fe7ed7fc85438664df9418b10c15a3f17d86729bb267baca234618
+size 4128
