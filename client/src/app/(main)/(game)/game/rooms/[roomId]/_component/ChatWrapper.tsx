6204f6fbe0f91c431e58c44b8f410c755665490f
이지원 <dlwldnjs950@naver.com>
2024-05-13 18:09:43 +0900

    refactor(fe): 방 입장 요청 방법 변경
    Former-commit-id: f20c17088e2951253258ce9c468598d2c7f00057

--- client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6a2f6e438665d39b29a272e0195d07a822c8f0798f4b3f733b0d34f551efac39
-size 3536
+oid sha256:1b4916b939cae7852d9d4e4a680eeb34b8a580601c351221a6613aec510d08ad
+size 3542
