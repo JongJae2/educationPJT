9aaf9cc7b65b0e61da5f8fbb12d61bfae3e2e5b8
최정윤 <janet971015@naver.com>
2024-05-13 21:45:07 +0900

    feat(be): RoomPlayer LocalDatetime String 값으로 저장
    Former-commit-id: af48d22d0df3c411593db1b34eb0bceee702ecb3

--- game-service/src/main/java/com/a508/gameservice/game/domain/RoomPlayer.java
+++ game-service/src/main/java/com/a508/gameservice/game/domain/RoomPlayer.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d510bb5d697d331f214ca3281ac3e5fa56f483ed2d58db240038b494a327776d
-size 470
+oid sha256:08358d52ffe4c912938143fd05b718d9cb7220eafbaa1619957565092328314b
+size 461
