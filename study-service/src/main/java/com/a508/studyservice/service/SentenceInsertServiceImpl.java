190faa955760c2110e416e9343445dbb7b7c4557
이종재 <ljj452@naver.com>
2024-05-13 12:41:37 +0900

    feat(be): feign 요청 기능 추가
    Former-commit-id: 255b763158292b8ea5498a658247688d701e1a98

--- study-service/src/main/java/com/a508/studyservice/service/SentenceInsertServiceImpl.java
+++ study-service/src/main/java/com/a508/studyservice/service/SentenceInsertServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e79f5cb6e79b6fd5c190fb96c0333b2f39b3fa7b2afdb684f930b42b1481652b
-size 3617
+oid sha256:864720dfbf635803348abdac5ab2a89f4302dda27eb17027720e78fe9c16a740
+size 3790
