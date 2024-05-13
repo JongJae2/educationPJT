2b4616c703cf4345dadd17055eb6b71a856717aa
하재률 <wofbf2@gmail.com>
2024-05-13 13:38:00 +0900

    fix(fe): timer초기값 설정
    Former-commit-id: 2b2e7c64255431a4f20160ba63603162ec686159

--- client/src/app/(main)/(home)/mypage/layout.tsx
+++ client/src/app/(main)/(home)/mypage/layout.tsx
@@ -1,3 +1,20 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e0a5195155079d3823c9f0b988a1ecf3b43ad906d3758f19edd467487a68e335
-size 379
+import MyInfo from './_components/MyInfo';
+import MyPageNav from './analysis/_components/MyPageNav';
+
+export default function MyPageLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <div>
+        <div className='text-xl py-5'>
+          <MyInfo />
+        </div>
+        <MyPageNav />
+      </div>
+      <>{children}</>
+    </>
+  );
+}
