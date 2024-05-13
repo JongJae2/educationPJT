270625809a83ed2596753414c59784bd7809d1ec
최정윤 <janet971015@naver.com>
2024-05-13 10:50:13 +0900

    refactor(be): user와의 통신 header로 변경
    Former-commit-id: cc3ee740c01a79ec61e5f932d92b5412cc885375

--- game-service/src/main/java/com/a508/gameservice/game/service/UserServiceClient.java
+++ game-service/src/main/java/com/a508/gameservice/game/service/UserServiceClient.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e135861cee3aaf2f6ff74deb2f2a9d07319bf77f5503c433609890c936849774
-size 844
+oid sha256:5fac109f0413c188a68bef70507dcec70da0dd4ec760b6aa6da06ed24560f4b9
+size 663
