29e4517f2f2a5c27fe419a603f84502987bdabfe
이종재 <ljj452@naver.com>
2024-05-13 15:39:25 +0900

    style(be): 불필요 코드 제거
    Former-commit-id: 3378f37570af8b1b4df43f0a9e636dc55be908bf

--- study-service/src/main/java/com/a508/studyservice/controller/ProblemController.java
+++ study-service/src/main/java/com/a508/studyservice/controller/ProblemController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c3c968cedfbbbd020f82a831c528e90953cec6adb9e8eb7f8491a1ae4f8666c0
-size 5565
+oid sha256:ea65695d90556932860dcce2a0b0f377989d9935fbcfea8aad6137277e25e5c2
+size 5506
