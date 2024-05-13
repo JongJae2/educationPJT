a8bc40fc5e555d6e1c4ae457d745177ab83b382b
강지헌 <wlgjs0458@naver.com>
2024-05-13 09:53:56 +0900

    fix(be): dic 설치파일 위치 수정
    Former-commit-id: 8d4351d92a712b2a4bd08c90cc5aa20a80434742

--- recommend/recommend/app.py
+++ recommend/recommend/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb075d30039c3550dab2d6c49ccc523cf4fbfdb6d7c3767017e3bd9be92f164a
-size 4451
+oid sha256:311b942e94c3765408625a803e9d45fc24ac54ff225ec9cb41f6ea9c5949a36b
+size 4458
