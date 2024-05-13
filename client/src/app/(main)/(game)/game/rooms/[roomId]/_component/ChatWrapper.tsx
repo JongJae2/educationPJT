403ba31c09c0fca46a8a633859c952213066a89e
하재률 <wofbf2@gmail.com>
2024-05-13 20:28:02 +0900

    fix(fe): 대기중 정답외치면 정답처리되는 버그 수정
    Former-commit-id: 6502fc0d8afcb8ab3e577f506ba22169aea58dbb

--- client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:281bba038d15219461cae15f224ee170240efea8cf445642a93082c3fd80fd92
-size 3809
+oid sha256:2a572201c2dd8e7e0e8aa2ee27d9ea7296c690d0a83e80a5b7dce01d21a993c8
+size 3810
