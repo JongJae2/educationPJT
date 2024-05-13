14dfb247426a6ae20e53e1fae3405f89c35c61ad
이지원 <dlwldnjs950@naver.com>
2024-05-13 14:53:20 +0900

    design(fe): 이미지 변경 예정
    Former-commit-id: 2392f54729f034ee37f0af7cb26c2eb325dbded6

--- client/src/app/(intro)/(about)/page.tsx
+++ client/src/app/(intro)/(about)/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13af60bb0a243b6de4f9061c9d0c495f5e9f67633aaf9885b5bbcf4b653fdca4
-size 9918
+oid sha256:45a229cf60fab34d40ef97debfe458a6d14000c0a14a924c49855f8fc437494e
+size 10033
