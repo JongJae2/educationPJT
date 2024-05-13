75b882f5b36b901f7e99c2aa1d8e5a028a161a7f
이지원 <dlwldnjs950@naver.com>
2024-05-13 18:52:06 +0900

    fix(fe): 이전 퇴장 메세지 출력 해결
    Former-commit-id: 3c25fd8977dea471597e5d071d7a486b590cb9a6

--- client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1b4916b939cae7852d9d4e4a680eeb34b8a580601c351221a6613aec510d08ad
-size 3542
+oid sha256:dc46d17b21ddcc835f714c4943235a5a8e4ab65f114ce02a86a7e29a09b4c579
+size 3650
--- client/src/stores/websocketStore.ts
+++ client/src/stores/websocketStore.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ade03e8957e56058047e2c483e625ea420bad8ed8b9dcc60a188633658adedf5
-size 4784
+oid sha256:7ad149ed3dbb9d3e4f506f5c199d087745942c2a092635936a212698329d290b
+size 4838
