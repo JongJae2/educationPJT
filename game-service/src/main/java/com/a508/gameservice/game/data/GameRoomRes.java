f6494a7ba21255551a6c7e36b51dbfe79dbaa713
최정윤 <janet971015@naver.com>
2024-05-13 09:16:56 +0900

    refactor(be): 방 반환값 수정
    Former-commit-id: 2b2d940bc9d34a4aee3a316a4915d85570b75cad

--- game-service/src/main/java/com/a508/gameservice/game/data/GameRoomRes.java
+++ game-service/src/main/java/com/a508/gameservice/game/data/GameRoomRes.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8a13baeedb548baab647861296da0305a01591f26f07f18bb3971a0ec725dbda
-size 586
+oid sha256:cf9162185c65f0358ee90d00de62a050e2f65ec1e598c869f9e08d8d25b59f76
+size 555
