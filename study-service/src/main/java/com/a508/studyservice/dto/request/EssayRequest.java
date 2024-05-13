62c839fe26bba58cd1f55e3a1a78a5d8a494070d
이종재 <ljj452@naver.com>
2024-05-13 11:06:32 +0900

    feat(be): TodayTopic token 로직 추가
    Former-commit-id: 86265198090d6be347ddd822860cf3e0cf8c5396

--- study-service/src/main/java/com/a508/studyservice/dto/request/EssayRequest.java
+++ study-service/src/main/java/com/a508/studyservice/dto/request/EssayRequest.java
@@ -1,15 +1,3 @@
-package com.a508.studyservice.dto.request;
-
-import lombok.Data;
-
-import java.util.List;
-
-@Data
-public class EssayRequest {
-    String type;
-
-    List<String> myAnswer;
-
-    List<Integer> problemId;
-
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:16c7dabbef61b5dd3edf2b236c4d95eb61f8f2cfdccc00306cef461682912913
+size 224
