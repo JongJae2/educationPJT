e5bbb225775e87ead34b62b0c8708bc7250c6964
최정윤 <janet971015@naver.com>
2024-05-13 13:04:58 +0900

    feat(be): myPage와 연동
    Former-commit-id: b80a6347e8c3b2bad8ffeb65ed0091cb77cf8409

--- game-service/src/main/java/com/a508/gameservice/game/controller/GameRoomController.java
+++ game-service/src/main/java/com/a508/gameservice/game/controller/GameRoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1dc2d7f677ab4b84f35235491db783d5887c86d5d47f19f96de0e8d76406a8a2
-size 2007
+oid sha256:8b180ec41692e3339a8a1809621302516b8301cd98f0048cce4fe9cdd504813b
+size 2076
