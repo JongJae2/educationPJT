3608feb03f8b80704a6d60ef2df4192a3f6738b0
littlebird <dlwldnjs950@naver.com>
2024-05-13 23:04:49 +0900

    fix(fe): no underlying STOPM 클라이언트 오류 해결
    Former-commit-id: 1054562f49139e79f144255df0a8271ed0597869

--- client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/_component/ChatWrapper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a14971f2b97d2a0bfc25dae8e0dad2b560e258e3d7d796bd6d7ec6aa0202dba1
-size 3938
+oid sha256:c033f4eada3520964c7a9cfda2240c8f5da66e4170be30b08cd2933782f69cd3
+size 3812
