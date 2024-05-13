363aa6a623a41c124aa3bf81b0b0baa627645c7b
이지원 <dlwldnjs950@naver.com>
2024-05-13 12:42:43 +0900

    fix(fe): 빈 배열일 때 예외처리
    Former-commit-id: 76329e02f9f9a750223d2294b98cb4abbcc12400

--- client/src/app/(main)/(game)/game/rooms/[roomId]/_component/GameScoreBoard.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/_component/GameScoreBoard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:65ce11691c4c77822f2838ad37937c58e9751938a1b426d2e195720a9d58d38b
-size 1284
+oid sha256:cd3ae4432e5c385a5d14aa81f87d5362c0e20a683dd94fd6ca04b343be78fd94
+size 1290
