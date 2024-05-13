b3e8198ac20a7990c244f3fb09ab3c7f96769a88
강지헌 <wlgjs0458@naver.com>
2024-05-13 10:22:54 +0900

    fix(be): elo 수정
    Former-commit-id: c44d1af3ca0a7769c98838633dfd6f0457f55291

--- recommend/recommend/app.py
+++ recommend/recommend/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c8138a3aae8ca0bfd8856a2f1f6a557f053863db2af014d4f697214e9ff74f37
-size 4513
+oid sha256:6ba5609bc7602a56339f350cdd2d46b17b1febeca3935bf138147527f2682808
+size 4576
