8d903139c65e1429bb3aa2b59a4f993b9705cb74
하재률 <wofbf2@gmail.com>
2024-05-13 19:18:32 +0900

    fix(fe): 웹소켓 버그 수정
    Former-commit-id: 225a986fb9b5e6a51e9a8875865e769d24282714

--- client/src/app/(main)/(game)/game/rooms/[roomId]/page.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3f3b9cdf36bcfcf18ee2676c86317a6a7d72a0782ec09e087ecb9d4013fb372f
-size 1815
+oid sha256:cfe9d2f3ef2f98de7ea6f25a74670cdac2b15143c859ec0fae47ba36a88816a8
+size 1867
