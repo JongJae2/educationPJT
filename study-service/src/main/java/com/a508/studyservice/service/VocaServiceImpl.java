3109ea66879dbbadec3e67388429e9f6750da529
이종재 <ljj452@naver.com>
2024-05-13 12:41:37 +0900

    feat(be): feign 요청 기능 추가
    Former-commit-id: 7c955f37373116a48cef986b9af9acf1efc44205

--- study-service/src/main/java/com/a508/studyservice/service/VocaServiceImpl.java
+++ study-service/src/main/java/com/a508/studyservice/service/VocaServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8537b3a31caa293ff5a25994edacab64df69be73ee5854f6e37922848dc51cf5
-size 3195
+oid sha256:e48dc5cb00386406ade532d006cb5ebec74f937451a16844781e6a1d1ce692d5
+size 3378
