0491e47f3ef7806d8c254c4fd156fdac3f1ba7b3
하재률 <wofbf2@gmail.com>
2024-05-13 15:17:34 +0900

    fix(fe): 게임 상태
    Former-commit-id: c5406b22173b3be325b8e0b5d0d4706953c05248

--- client/src/app/(main)/(game)/game/rooms/[roomId]/_component/TimerWrapper.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/_component/TimerWrapper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18fd8e53f5ab0dc4b46199acf517f572ee5f3ccf4d6f8feee2ffe6b033b5aab3
-size 1245
+oid sha256:87ebab781732c515e2f7df76396110510b6485729c307dd65e9c5bc645610ec0
+size 1327
