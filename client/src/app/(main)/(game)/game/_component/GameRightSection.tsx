0a4400f5f832b5ebf8d9afdc30c101a9e791f612
이지원 <dlwldnjs950@naver.com>
2024-05-14 10:48:40 +0900

    feat(fe): 새로고침 버튼 동작
    Former-commit-id: 53a59d44cd6d296d5b823b4d8b8d5beb99a2c18b

--- client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
+++ client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a4c7fa8b2a1a80a2fa899126a051e261dce12f053f173325bdb6f1aabc30de1
-size 3871
+oid sha256:19c6b74c5e9457de8545c38c5edc94a149d9fb4e625f4690d65f6d07b72b715c
+size 3930
