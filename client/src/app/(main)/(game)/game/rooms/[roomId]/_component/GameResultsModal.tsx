7db63679ff859a3bd11b25739f165df289be144d
이지원 <dlwldnjs950@naver.com>
2024-05-14 18:12:46 +0900

    feat(fe): 결과 구독 됨
    Former-commit-id: 71def8f3d37f0f18b1d4ae0f237ecd58e488054e

--- client/src/app/(main)/(game)/game/rooms/[roomId]/_component/GameResultsModal.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/_component/GameResultsModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b2302ebd8cdcdbdf2f3fb10b7398ed6cbc0604f2f629eef4c7e35b6712cd6200
-size 2629
+oid sha256:63f8957c7d8c529249c8d62c850a4667cabe271b053653d6dbfd3101214352ec
+size 3311
