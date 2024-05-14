d0ea63b5fe8bd7f229b73d49641581c1a89d93d9
이지원 <dlwldnjs950@naver.com>
2024-05-15 01:51:42 +0900

    fix(fe): 방장 바뀔 때 게임결과 요청 오류 해결
    Former-commit-id: 658a7ac81447bc57a219e7c035cc7bea205784d4

--- client/src/stores/websocketStore.ts
+++ client/src/stores/websocketStore.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eb722f325051fe856ca04611a1138568888760a1fa297127a6ba385fdefb2a5f
-size 6881
+oid sha256:8a5aabeb06ae131380643607759092437da0c829465c98f8d3ec613221bea7b4
+size 6963
