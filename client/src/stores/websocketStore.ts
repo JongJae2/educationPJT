4de527ed1ebbcc871dc8291e61c171f84924e6bf
하재률 <wofbf2@gmail.com>
2024-05-14 15:23:01 +0900

    fix(fe): 웹소켓 연결 끊기
    Former-commit-id: 1013ea6463958f6ee927b3fe4fc5f0c3e42433f2

--- client/src/stores/websocketStore.ts
+++ client/src/stores/websocketStore.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1779c98ce381ea2a449b4757968ec03eea4dd81be65058dd6fa189067c269e41
-size 6011
+oid sha256:e4371803a009451f72428b9f6e9ed3e1832ba001c0dfe7bb53552495667ea5d5
+size 6396
