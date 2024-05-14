b884ba7f5abcda9ad8c9bcec7722b7b495d981da
이종재 <ljj452@naver.com>
2024-05-14 18:58:58 +0900

    fix(be): token 로직 수정
    Former-commit-id: dc9d12033ccc53a2e6252a9f96edcc03ba992609

--- study-service/src/main/java/com/a508/studyservice/service/TodayLearningServiceImpl.java
+++ study-service/src/main/java/com/a508/studyservice/service/TodayLearningServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f32d523fb0315532c6bf425e1e0e153613da795e04ae95da6e56ff685cbac0b8
-size 15199
+oid sha256:bf05808f364cf4cc9845e62e6a265b7ad01e6c904416ff91824f679b7fc8aa4c
+size 15361
