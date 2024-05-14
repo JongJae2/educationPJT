7ae1817314ef681ff9037f89ff31839e46a5fa1f
최정윤 <janet971015@naver.com>
2024-05-14 09:48:24 +0900

    feat(be): 방장 정보 전송
    Former-commit-id: bfa768058d8a89c8af4ea4ce986df90404caed87

--- game-service/src/main/java/com/a508/gameservice/game/data/MessageType.java
+++ game-service/src/main/java/com/a508/gameservice/game/data/MessageType.java
@@ -1,8 +1,3 @@
-package com.a508.gameservice.game.data;
-
-
-public enum MessageType {
-    CORRECT, ENTER, EXIT, TALK, QUIZ, ANSWER, START, END
-}
-
-
+version https://git-lfs.github.com/spec/v1
+oid sha256:dd133ade5c3694add1542679cc97a50454a62bb2ca02f07852002e0b8bf54224
+size 138
--- game-service/src/main/java/com/a508/gameservice/game/service/GameRoomService.java
+++ game-service/src/main/java/com/a508/gameservice/game/service/GameRoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:60a1b71068775aed47f43edc8a2f4385a2f0e1224e34305c7272a3bc836644bd
-size 15294
+oid sha256:152f91302968faa944fca66993beace5d9a74b3518c92a72a4f625f5f2652c2d
+size 15687
