145e42b4d4fe91f8f119820b4980295a841b10bf
이종재 <ljj452@naver.com>
2024-05-14 10:26:16 +0900

    fix(be): 토큰 로직 오류 수정
    Former-commit-id: 88de65cdb453e33a2b7661aa53e1a7ab31c48cd7

--- study-service/src/main/java/com/a508/studyservice/service/ChoiceSolvedServiceImpl.java
+++ study-service/src/main/java/com/a508/studyservice/service/ChoiceSolvedServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4315adbe501e38568ec20408e1bec4a092f559d360cfc56a304b015dd78f5296
-size 6822
+oid sha256:9567e6d8270a0b64de82c8cf71219a270fd13d55620e2a87800adc7186bcad41
+size 6958
