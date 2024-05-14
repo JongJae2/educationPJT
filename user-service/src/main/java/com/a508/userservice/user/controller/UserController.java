a82bf9ab812d91c56bedc834cf27c4d33312b6a7
강지헌 <wlgjs0458@naver.com>
2024-05-14 14:45:47 +0900

    fix(be): token 수정
    Former-commit-id: ddcd738f708b73934ba2066b142dc52135ad26c8

--- user-service/src/main/java/com/a508/userservice/user/controller/UserController.java
+++ user-service/src/main/java/com/a508/userservice/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba00039c5db38e59488e8e5e2343abe536d26337df7d51de33af27538fa4c699
-size 6170
+oid sha256:b19e1f5ceba75081cf8ab7349914565f6e4f20b316111732a5ec9da871612b53
+size 6157
