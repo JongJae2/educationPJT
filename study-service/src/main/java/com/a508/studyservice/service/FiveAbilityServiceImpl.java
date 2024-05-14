40941e00053bf7476e9242e142a034901efe5bb7
이종재 <ljj452@naver.com>
2024-05-14 10:16:01 +0900

    feat(be): 전체유저 5가지 능력 평균 api 생성 및 구현
    Former-commit-id: 4608718c027eb77e6c813a429fc8e436c19d1da1

--- study-service/src/main/java/com/a508/studyservice/service/FiveAbilityServiceImpl.java
+++ study-service/src/main/java/com/a508/studyservice/service/FiveAbilityServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:afdd5251c0c4b9452800c61579c59df9ac3a3c2a1dd28514fe9c26637c7f1f85
-size 1471
+oid sha256:138683dbc4b9d2db55396382a6993259b150905aaec772fa6ca370023b645830
+size 2326
