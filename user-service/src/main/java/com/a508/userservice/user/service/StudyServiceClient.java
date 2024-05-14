3388d58ffcc5ebdb8922f79edf1b42a396f07d8a
강지헌 <wlgjs0458@naver.com>
2024-05-14 15:57:23 +0900

    fix(be): token 전달방식 수정
    Former-commit-id: d3b7d0365ea5b4266a72c292d105292621d67769

--- user-service/src/main/java/com/a508/userservice/user/service/StudyServiceClient.java
+++ user-service/src/main/java/com/a508/userservice/user/service/StudyServiceClient.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d3a707b9cb1d5571a52ed560842dd66208423607d051a3f6568db039a456aed2
-size 543
+oid sha256:6f4f45ca57907d06406bc19ed63ab71d72da6ff941a0f635b416797eb83d93b7
+size 551
