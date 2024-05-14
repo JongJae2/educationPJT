6305e88358457713e019b88c07afde37ac3c2aa1
이지원 <dlwldnjs950@naver.com>
2024-05-15 03:55:30 +0900

    fix(fe): error
    Former-commit-id: 10ef0cbd01b103f0ee6ef0dd4999e1a819c0d248

--- client/src/app/(intro)/(about)/page.tsx
+++ client/src/app/(intro)/(about)/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:20077a659497b149bf7cfb4bf7c0820924f9505eba4a4efbdcd77dfb555bfa0f
-size 10510
+oid sha256:a97719996dbf53c6edb35150cfba5f227d7c112c6e672731dc6721ad8f86da9f
+size 10511
