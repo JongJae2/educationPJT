c3199d40009171b2a1138662c89d3136a5a953e1
이종재 <ljj452@naver.com>
2024-05-14 18:36:14 +0900

    feat(be): 월, 일 데이터 서비스 로직 생성
    Former-commit-id: 89fc6075ffb7ea7a50e5459e6db31b0d9a05f463

--- study-service/src/main/java/com/a508/studyservice/controller/SolvedController.java
+++ study-service/src/main/java/com/a508/studyservice/controller/SolvedController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5348e9751328ccd2662513702b035672def4da5eb73ce157f69c5a2fb7b78cce
-size 3292
+oid sha256:b4511b43c27e860c562afb49264c5b28585704cd9ae3d0299b914dc26897f117
+size 4113
--- study-service/src/main/java/com/a508/studyservice/service/TodayLearningService.java
+++ study-service/src/main/java/com/a508/studyservice/service/TodayLearningService.java
@@ -1,11 +1,3 @@
-package com.a508.studyservice.service;
-
-import java.util.List;
-
-import com.a508.studyservice.dto.response.TodayLearningResponse;
-
-public interface TodayLearningService {
-
-
-	List<TodayLearningResponse> getTodayLearning (String token);
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:db81d0a186f5b7f888d5cab1fe1e5726f465d93c64b8474f49321e2a1a8d2eb7
+size 644
