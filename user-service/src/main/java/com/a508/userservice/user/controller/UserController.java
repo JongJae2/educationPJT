1c4b17c273c1ef4abb0cf3c18be1d4f0cdb701b4
강지헌 <wlgjs0458@naver.com>
2024-05-14 16:11:31 +0900

    fix(be): data 전달방식 수정
    Former-commit-id: d8dde9ef575addfe6404a15e9cf9911c2da83efd

--- user-service/src/main/java/com/a508/userservice/user/controller/UserController.java
+++ user-service/src/main/java/com/a508/userservice/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83488c55c516808519ebd80cabcb0d43c71134f2d002336a814cbe818bc2df5b
-size 6301
+oid sha256:5ebe3fa8d77bdd3557fc0588f2300532413910fcceb1a767cf05972b0882d81e
+size 6265
