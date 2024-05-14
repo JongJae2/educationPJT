854cdba69c5724c5feaec45474deafb47e883fe3
이지원 <dlwldnjs950@naver.com>
2024-05-14 16:18:01 +0900

    feat(fe): 대전 결과 요청하기
    Former-commit-id: 87b32a91a6d39a6985cf539c16e382c3e0e75cdd

--- client/src/app/(main)/(game)/game/rooms/[roomId]/_component/GameResultsModal.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/_component/GameResultsModal.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:894939093a83a206ec269d47e8469e481c4c57ab5badb1a1924c88c47286b4a2
-size 2382
+oid sha256:b2302ebd8cdcdbdf2f3fb10b7398ed6cbc0604f2f629eef4c7e35b6712cd6200
+size 2629
