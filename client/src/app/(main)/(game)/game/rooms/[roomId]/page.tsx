50e539753d3f60f511bfe32ae94d17905281ee7a
하재률 <wofbf2@gmail.com>
2024-05-14 19:31:18 +0900

    fix(fe): url로 접근
    Former-commit-id: fe088622cd827be488f7a8ec577516879e4d84d2

--- client/src/app/(main)/(game)/game/rooms/[roomId]/page.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4716e55ae3ddaf6a361f757b5c6ced37329d2e5d4b5a8c1e18bf63798d03b243
-size 2564
+oid sha256:93a7459689ac6d2fc719832ac2bd2880d63c491df425985896130a02ceec5090
+size 2650
