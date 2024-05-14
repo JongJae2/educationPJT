29c4a7a71e4603779ce4ba4b24ee1e53f7d570a8
권정훈 <dhtmxk8134@naver.com>
2024-05-14 13:06:23 +0900

    feat(fe): 순서배열 구현
    Former-commit-id: 532bf38cafb15039361cb875b45fe37dca3fafbe

--- client/src/app/(main)/(learn)/learn/order/_components/Order.tsx
+++ client/src/app/(main)/(learn)/learn/order/_components/Order.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e13a3335b234151dd765851ae0709991dcd314e7652bf442cc884d38271c383c
-size 11095
+oid sha256:f8e6bfcea7990fc78e0d157261eee2ea5c20d4222eae249a2619414cec4c170a
+size 10750
