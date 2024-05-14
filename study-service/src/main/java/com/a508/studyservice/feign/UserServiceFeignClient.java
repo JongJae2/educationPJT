2d0d2a5078f36ea1f530b4086152ae062ca4b5ba
이종재 <ljj452@naver.com>
2024-05-14 18:41:12 +0900

    fix(be): Feign 경로 수정
    Former-commit-id: 3ece8fffa5574153a5862fef2ba783dc9fa889c4

--- study-service/src/main/java/com/a508/studyservice/feign/UserServiceFeignClient.java
+++ study-service/src/main/java/com/a508/studyservice/feign/UserServiceFeignClient.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a0e789a2a96f1b32365726534bbade07bdc95612f0cfe7cea52928ecd619644
-size 585
+oid sha256:e93dc7ea8b5e8786a8888b31ee43f4f8151b6a8e5f2e183e0b73b0d151f86ff6
+size 586
