8e705ae4904c91140d0643b5a83b638915379aa3
권정훈 <dhtmxk8134@naver.com>
2024-05-14 10:08:46 +0900

    fix(fe): 코드 블락
    Former-commit-id: ae822ad17caac14f82b27cf484508af90db2166a

--- client/src/app/(main)/(learn)/learn/_components/ProgressBar.tsx
+++ client/src/app/(main)/(learn)/learn/_components/ProgressBar.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df8a1a2bde943b3e701d31f2319b82c024f083591f214b584582c9768c3a4565
-size 496
+oid sha256:9eb2958af3fc97b0c94e9e05d67206a7d351cfc1010b069f35fcfa5d496cec29
+size 509
--- client/src/app/(main)/(learn)/learn/order/_components/OrderResult.tsx
+++ client/src/app/(main)/(learn)/learn/order/_components/OrderResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4bd71d7ede90571226f2dfdea93cc71af00b07adc200fe6c8b1c590b335e641
-size 8233
+oid sha256:fe409bad0c31d535bbb0f351fc7369879aec7dfea5589f294625c2f3a9b6fa6c
+size 8241
