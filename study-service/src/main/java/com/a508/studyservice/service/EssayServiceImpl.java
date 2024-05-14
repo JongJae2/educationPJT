ba3775d4adf5791aba7922477f7f3bc58081c1a7
이종재 <ljj452@naver.com>
2024-05-14 14:56:34 +0900

    fix(be): 오늘의 학습 수정
    Former-commit-id: bf94028e55b105692e8396da2a12343608958c12

--- study-service/src/main/java/com/a508/studyservice/service/EssayServiceImpl.java
+++ study-service/src/main/java/com/a508/studyservice/service/EssayServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e62a51d79eeffafdaf58ffe0e60f0ca3a29b5adcf5002f7d22700b2bad9e2a04
-size 7668
+oid sha256:9f9865c6937bfa28a025d138b419b668b5e9a3d4c16a3da9080b274364fa0230
+size 7800
