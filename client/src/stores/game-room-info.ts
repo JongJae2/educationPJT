3a24dabd7f0510e7679747e1c0d1f3a3f5b8c641
이지원 <dlwldnjs950@naver.com>
2024-05-15 01:38:40 +0900

    feat(fe): 방정보에 방이름도 담음, 게임결과 요청, 게임결과 화면
    Former-commit-id: 1d801093c93073fbc79327e0bb4c29dc5245983c

--- client/src/stores/game-room-info.ts
+++ client/src/stores/game-room-info.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fa54cdbf43eb2732e055544c33ba27bced8daf0b7fb55f8cc874aae44bdc41bc
-size 1635
+oid sha256:bcc055f9cd4d4a30494c04fd5d03248d60e33990bbc3e074a541515e14321e52
+size 1722
