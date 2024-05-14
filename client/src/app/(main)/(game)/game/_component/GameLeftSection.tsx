1a41242e97b58edc2bfefaedb94463e041e1c865
하재률 <wofbf2@gmail.com>
2024-05-14 16:16:04 +0900

    refactor(fe): 데이터 없을때 화면, 랭킹 순위권 밖 처리
    Former-commit-id: 786b4b9147bac0e4f1e59e73222dd7662b23b4c7

--- client/src/app/(main)/(game)/game/_component/GameLeftSection.tsx
+++ client/src/app/(main)/(game)/game/_component/GameLeftSection.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df33782f36e83bf0c00db29c9bbaf2360fb98f537714bb5c1a066d4fb8cf932e
-size 3889
+oid sha256:897c748cb0a9f0d52c07b4cd6577cbfb67b2471d61ed7f6166ddb82a51ca2175
+size 3967
--- client/src/stores/websocketStore.ts
+++ client/src/stores/websocketStore.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e4371803a009451f72428b9f6e9ed3e1832ba001c0dfe7bb53552495667ea5d5
-size 6396
+oid sha256:dd2a96542e46513e96dbbc55aae20fef24cdbf77eeb4c97d5a7237b5788e6a82
+size 5927
