a0b21753d915780d9139709d807e30e6453e19c1
강지헌 <wlgjs0458@naver.com>
2024-05-14 16:29:18 +0900

    fix(be): data 전달방식 수정
    Former-commit-id: 54b1bf98af629a53694104bcf9049fea689416b1

---
+++ user-service/src/main/java/com/a508/userservice/user/data/fiveDto.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ce9e31686ec632b8cfe6d76146729a7c8c370ce5622ebfe55da88a4bd3bd421b
+size 238
--- user-service/src/main/java/com/a508/userservice/user/service/StudyServiceClient.java
+++ user-service/src/main/java/com/a508/userservice/user/service/StudyServiceClient.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8d7040f6cdab3f104c158a12f64534fa8ddaf4dadd406827caaa8d2111cae419
-size 638
+oid sha256:06ac654b08a46cdc1c0fa25d4abfa98bef17ad0d9d5d49e66f7c79e56a192715
+size 645
