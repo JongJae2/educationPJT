4ed986f26721b0b43e1398831c5d26f4487ddb19
강지헌 <wlgjs0458@naver.com>
2024-05-14 16:20:46 +0900

    fix(be): data 전달방식 수정
    Former-commit-id: 9085649bc2e896837b612f16645efd03b52203a9

--- user-service/src/main/java/com/a508/userservice/user/service/StudyServiceClient.java
+++ user-service/src/main/java/com/a508/userservice/user/service/StudyServiceClient.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8acbc13210b119d9e5940adf3b272d58a4edf81a2f2f4c8a989ded67a20bf0ac
-size 621
+oid sha256:8d7040f6cdab3f104c158a12f64534fa8ddaf4dadd406827caaa8d2111cae419
+size 638
