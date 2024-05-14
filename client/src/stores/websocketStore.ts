bf595ed3027ddbdcc94d84b1db9875fe002e72ca
이지원 <dlwldnjs950@naver.com>
2024-05-14 19:08:17 +0900

    fix(fe): 게임 결과 하는 중
    Former-commit-id: 9cff99c142551332c48bafb8bb42fe01e9476cd6

--- client/src/stores/websocketStore.ts
+++ client/src/stores/websocketStore.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bd22962d247c162f3f439644a100db64df20aa6d7f1844d60cc3a65accb0cd80
-size 6658
+oid sha256:0c1df966f5f2b182475633744e386756d50d546c90476b754b19951a721cd77d
+size 6721
