80cd323bb79357b9f82f568d50cd219f5b624287
이지원 <dlwldnjs950@naver.com>
2024-05-15 03:50:36 +0900

    design(fe): 소개 페이지 사진(교체예쩡)
    Former-commit-id: fe12ca58bcf0a77b52c2fa9261a00c6f1df03c28

--- client/src/app/(intro)/(about)/page.tsx
+++ client/src/app/(intro)/(about)/page.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:45a229cf60fab34d40ef97debfe458a6d14000c0a14a924c49855f8fc437494e
-size 10033
+oid sha256:20077a659497b149bf7cfb4bf7c0820924f9505eba4a4efbdcd77dfb555bfa0f
+size 10510
