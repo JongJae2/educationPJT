cde0da5433745a18023f193a1489bd89245da5d3
최정윤 <janet971015@naver.com>
2024-05-14 17:35:06 +0900

    feat(be): errorcode 생성

--- apigateway-service/src/main/java/com/a508/apigatewayservice/exception/CustomException.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:b2eb59a726f1e9eded36de832930798038050fbbefe6625741148215918c5446
-size 240
--- apigateway-service/src/main/java/com/a508/apigatewayservice/exception/ErrorCode.java
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:372e3bcf943a984bb43db6fb8c6810bc4c4d274f1c177a1c641e763b969feaeb
-size 533
