eed0a593eef776de6a797bc0385ae5ac8732b11a
최정윤 <janet971015@naver.com>
2024-05-15 17:49:46 +0900

    feat(be): 빠른 입장 api 구현
    Former-commit-id: 3a2c6ed5cefb9e9d0a544582c43f87bcf8f44804

--- game-service/src/main/java/com/a508/gameservice/game/controller/GameRoomController.java
+++ game-service/src/main/java/com/a508/gameservice/game/controller/GameRoomController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:54a065008b35a5da8c4f5028bf3b14c34a0ccade790a29d8b138111e312adf12
-size 3237
+oid sha256:03be2810d319454ca26a5d0cb197cc8d121188247eacdd2b483b80256d8e594b
+size 3242
