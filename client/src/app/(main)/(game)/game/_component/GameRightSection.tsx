9709955a3143bccf8071e9376abf4935d906f965
이지원 <dlwldnjs950@naver.com>
2024-05-15 19:24:58 +0900

    fix(fe): 방이름 저장 로직 변경
    Former-commit-id: bdc9d854c1735a0c01929cdf666d4d36af17253b

--- client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
+++ client/src/app/(main)/(game)/game/_component/GameRightSection.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a9f4835640c7933338fd6942fd310743cb065c7ecc8e9e420126446ab48539ca
-size 4411
+oid sha256:1d76760bb4d1b09b092e418dbd0137654ccfd7b921a2aa632d3dd220484a6953
+size 4377
--- client/src/queries/get-room-info.ts
+++ client/src/queries/get-room-info.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a81140e34387555e09c87fbc53881b2abfb1fa274077d3abd1e1ad69203e74f1
-size 1074
+oid sha256:592ab5a833bb00fa3a21938b203c12a24b04712f4fbced8eb61b798dd04c1e31
+size 1106
