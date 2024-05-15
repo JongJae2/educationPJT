69de9be619333acad9e582dc10efdb1c09dbb453
최정윤 <janet971015@naver.com>
2024-05-15 17:14:06 +0900

    feat(be): 빠른 입장 관련 메소드 생성
    Former-commit-id: 69e6c6a1ccff7581a95e8d1cc49e6695eeb86419

--- game-service/src/main/java/com/a508/gameservice/game/repository/GameRoomRepository.java
+++ game-service/src/main/java/com/a508/gameservice/game/repository/GameRoomRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a4e86cc3a5b9aee6048f17032ab17c775b8d8c36709f2c4ef90ee50355da9992
-size 3180
+oid sha256:1b18428a152a3fcc36e71681be8843c040cd2d1118bf4f39e78ae55c2619ad49
+size 3651
