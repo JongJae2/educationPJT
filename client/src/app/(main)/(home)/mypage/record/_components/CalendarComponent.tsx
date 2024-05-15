5764a706e4e7a1b8a1d121ad6a9f21830f32a745
이지원 <dlwldnjs950@naver.com>
2024-05-15 22:20:09 +0900

    fix(fe): 다른 페이지 갔다오면 달력-기록 싱크 안맞음 해결
    Former-commit-id: 57b9f9498a5c1d0be11f592528fe5e9437b8dedd

--- client/src/app/(main)/(home)/mypage/record/_components/CalendarComponent.tsx
+++ client/src/app/(main)/(home)/mypage/record/_components/CalendarComponent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4855be6d9cd9358b6072cfecb9881c6e672c1a16d5d93d2f3ac6bd24b1ac3bd0
-size 2131
+oid sha256:dbee3aab22a062380c118d11ca065bb0a7b3a0374bcd4cac1456e8793b967d56
+size 2316
