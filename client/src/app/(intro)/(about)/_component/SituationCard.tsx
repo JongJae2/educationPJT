199b07200754999f21954edfa32103a8d7694d80
이지원 <dlwldnjs950@naver.com>
2024-05-15 15:22:40 +0900

    design(fe ): 소개페이지 레이아웃 정리
    Former-commit-id: ddd70afd5730383d4349c6d02646311a1d5f3dc0

--- client/src/app/(intro)/(about)/_component/SituationCard.tsx
+++ client/src/app/(intro)/(about)/_component/SituationCard.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:742719f27f3fb0a9f744a9d91b5ce1055ad5a4c98e7e661e21aa5d813f26faf6
-size 1002
+oid sha256:e8c991fa69beae45647a1fb9ae5b7da06d8ebfbc73469303f98b62acd3dedeab
+size 1026
--- client/src/app/(intro)/(about)/_component/UpsideAnimation.tsx
+++ client/src/app/(intro)/(about)/_component/UpsideAnimation.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:070fc4c113d5f8fea72ba6bd51993ba7de160d5fd1829f85eb824316ba9cbe5f
-size 1633
+oid sha256:34f2ad659317bc5628184e96001299ddcdb7fd1f73f71bc0536750d0829418b3
+size 1656
