f9806e4cedbf9673d4601332e8951e4488b787a3
이종재 <ljj452@naver.com>
2024-05-15 23:41:19 +0900

    fix(be): 오늘의 학습 생성 시간 수정
    Former-commit-id: 620b0241d842a3f1a4ce6c3517ff93deccfbbe42

--- study-service/src/main/java/com/a508/studyservice/service/TodayLearningScheduler.java
+++ study-service/src/main/java/com/a508/studyservice/service/TodayLearningScheduler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc6e73a96c78bfdad587f9cc7bab4f764f8194163602202f56faf83da0678cd3
-size 8598
+oid sha256:310914b2d87965717e2a2923cc8ce40d89de93597ccbb99825e7bc5e9cfccc03
+size 8596
