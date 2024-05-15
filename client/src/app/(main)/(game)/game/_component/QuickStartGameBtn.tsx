6bb62c9b617e38e76673af0e57774f34c73d6059
이지원 <dlwldnjs950@naver.com>
2024-05-15 21:37:36 +0900

    fix(fe): 언제까지 돌지...
    Former-commit-id: 3d460e3c7cac8bfea4fad69338285f5ceb102cf1

--- client/src/app/(main)/(game)/game/_component/QuickStartGameBtn.tsx
+++ client/src/app/(main)/(game)/game/_component/QuickStartGameBtn.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:df69bd988874993086670cb3d7be102a6eaeff2f51e105d453c848427f5a9813
-size 2619
+oid sha256:f6e7ce182f55bcf991d85211285546f0c1590ccaea4f7fe53df24112c1d11c9b
+size 2737
