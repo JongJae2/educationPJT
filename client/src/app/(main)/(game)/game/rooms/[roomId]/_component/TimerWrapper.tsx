8e44637e1ab4b414f22609517846c91a1fe3ded2
하재률 <wofbf2@gmail.com>
2024-05-15 19:34:19 +0900

    feat(fe): 효과음 추가
    Former-commit-id: 1311f725549d967e03fec211192a00d8f98a96f0

--- client/src/app/(main)/(game)/game/rooms/[roomId]/_component/TimerWrapper.tsx
+++ client/src/app/(main)/(game)/game/rooms/[roomId]/_component/TimerWrapper.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:418b7336db9b57929acb21709d7ea80ecd8dbfbd71e504b606e188932781e2f0
-size 1914
+oid sha256:09d64f4504664486f82116aa151f00e2c01cf548485ad4fdf04d1a8404f16daa
+size 2755
