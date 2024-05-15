f8199c4c17f9d783f0d67bf6ba3fbe7ace7fb390
권정훈 <righthun.kwon@gmail.com>
2024-05-15 13:06:23 +0900

    chore(fe): import 구문 정리
    Former-commit-id: 8d646387989c608593f44f2380b8d82c5b3f7389

--- client/src/app/(main)/(learn)/learn/insert/_components/InsertResult.tsx
+++ client/src/app/(main)/(learn)/learn/insert/_components/InsertResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:724925f6af6a355ae22d40c70f4bd218c69a078fcbc15b06a5fa475edb8253d7
+oid sha256:860fe38aa1802bbb6c1a84c5d9881d8aa6b1eb805c2ce53097bd2cc9a789c82d
 size 4211
