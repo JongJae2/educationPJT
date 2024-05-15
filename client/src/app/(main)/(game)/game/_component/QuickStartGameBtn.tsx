d685674c120921ab2d24889692c5e4e58f13655e
이지원 <dlwldnjs950@naver.com>
2024-05-15 20:40:43 +0900

    feat(fe): 빠른 대전
    Former-commit-id: 376e6fbff053a2acb36bb8ab5cb8df67befedb07

--- client/src/app/(main)/(game)/game/_component/QuickStartGameBtn.tsx
+++ client/src/app/(main)/(game)/game/_component/QuickStartGameBtn.tsx
@@ -1,9 +1,3 @@
-export default function QuickStartGameBtn() {
-  return (
-    <>
-      <div className='bg-white/80 rounded-md cursor-pointer text-sm md:text-lg lg:text-lg w-full h-11 flex justify-center items-center border-2 border-white hover:border-ourRed duration-100'>
-        빠른 대전
-      </div>
-    </>
-  );
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:ad4eeb756210f026c7111d80ca2d252ac82605a3b4a578ccb8472727b96bd3db
+size 1831
