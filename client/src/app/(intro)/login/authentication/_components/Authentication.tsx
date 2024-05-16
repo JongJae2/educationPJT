9fe53e01e7c4f03df1d7b75e2d4e49158ed54b39
권정훈 <dhtmxk8134@naver.com>
2024-05-16 15:23:13 +0900

    refactor(fe): 쿠키 저장
    Former-commit-id: 67de2879cb84ed85898c216ad2b11a15e2070f10

--- client/src/app/(intro)/login/authentication/_components/Authentication.tsx
+++ client/src/app/(intro)/login/authentication/_components/Authentication.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:360c776bbafa8e257fa22aeef72022adb238d841e0deb8482d5d58235bf73c4d
-size 1705
+oid sha256:06230ee5e6c43349e32ade9ce86172c589a53479b28d19edb649a65b47dbca0b
+size 1734
