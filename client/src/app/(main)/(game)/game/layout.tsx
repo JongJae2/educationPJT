a5714e66f8ad45373978564048765bd0358be840
이지원 <dlwldnjs950@naver.com>
2024-05-16 23:27:34 +0900

    chore(fe): console.log 삭제
    Former-commit-id: 413ce963032751b23c619b560a156de9153435c6

--- client/src/app/(main)/(game)/game/layout.tsx
+++ client/src/app/(main)/(game)/game/layout.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec1310bfd4eac6da6768fb170547147708f522e993c7bcb6ddf78849bbc35593
+oid sha256:f47305bbb29e2c419cb0cd3f9f87b008f8aef7ce5852ebd1bbe549d64caf9296
 size 1108
