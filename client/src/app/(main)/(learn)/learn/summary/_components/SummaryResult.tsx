9e8673222c3d6a429c1ebcf92e452d668c207c50
이지원 <dlwldnjs950@naver.com>
2024-05-16 16:12:00 +0900

    fix(fe): 마이페이지에서 문제 결과 확인완료 => 기록페이지로 이동
    Former-commit-id: ea1f50a522ad5a2f584e3ffa19354d3d4e78ac07

--- client/src/app/(main)/(learn)/learn/summary/_components/SummaryResult.tsx
+++ client/src/app/(main)/(learn)/learn/summary/_components/SummaryResult.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e41af1768a2f6303c3f7a8b6517529b4e2dad4018cf652cc620946138b1675f9
-size 3945
+oid sha256:ead6373b22efc3d5a607e91fb5621dbad5bd72cae4a997e66bf3cc4bef974ba5
+size 4055
