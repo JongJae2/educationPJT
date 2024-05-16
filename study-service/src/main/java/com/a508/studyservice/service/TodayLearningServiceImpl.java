0773b622312e65eae3c4457612cbd85dfcb1aa31
이종재 <ljj452@naver.com>
2024-05-16 10:48:39 +0900

    fix(be): service 수정
    Former-commit-id: 282b3af425d686c8b533f094a906b32c68649fd1

--- study-service/src/main/java/com/a508/studyservice/service/TodayLearningServiceImpl.java
+++ study-service/src/main/java/com/a508/studyservice/service/TodayLearningServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9845de83670aa3abfd88b9f1d95d6de920455a70eb1a43f41d95b6a8026fd983
-size 15403
+oid sha256:421855861245472cffea81ee202c07acc6707d2cf31b12955741225c3e69db10
+size 15348
