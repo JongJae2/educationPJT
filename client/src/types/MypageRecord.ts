7166625350bd49b650e301e37f9ad790984e1228
이지원 <dlwldnjs950@naver.com>
2024-05-16 13:33:22 +0900

    fix(fe): 승률 응답 데이터로 출력
    Former-commit-id: f9f643fa65082927bc849e7343cb280c0e6f063a

--- client/src/types/MypageRecord.ts
+++ client/src/types/MypageRecord.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:068134dec2ae528b962442a43e77040ea23e72b67b74eadbed48eb869cd77df3
-size 2293
+oid sha256:edc91e84444340364bb8aacb3cbd05e738c9c22e183600dfc7a9b98fa9764101
+size 2308
