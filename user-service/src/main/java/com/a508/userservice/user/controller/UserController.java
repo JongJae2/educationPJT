1a11223eb4c4a983c72deddd6d472fe0dcfe0a84
강지헌 <wlgjs0458@naver.com>
2024-05-16 13:04:02 +0900

    fix(be): 캘린더 최종수정
    Former-commit-id: dfb489061042df0b04f5cdbeda77ea0158bd3dab

--- user-service/src/main/java/com/a508/userservice/user/controller/UserController.java
+++ user-service/src/main/java/com/a508/userservice/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c06321e73b7134614d964b2eba73c9b4d207d23173bd5ef27a5a10973710e717
-size 6648
+oid sha256:e1da11c981dd5d75a9feb31715aa80e204d79bede382840f3a4886cbe0f58a5e
+size 6427
