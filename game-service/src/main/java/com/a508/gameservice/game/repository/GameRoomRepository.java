40d97e8ebee05d96718caef8b458695aebed8eca
최정윤 <janet971015@naver.com>
2024-05-16 10:59:07 +0900

    refactor(be): 빠른 입장 예외처리 로직 수정
    Former-commit-id: 2c708afe8fde16b0d878586b29b24f00e2b8e556

--- game-service/src/main/java/com/a508/gameservice/game/repository/GameRoomRepository.java
+++ game-service/src/main/java/com/a508/gameservice/game/repository/GameRoomRepository.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24426b8539fd13b2fa7e9f082534ec124cce8b42f73965362cdced425157ebb3
-size 3759
+oid sha256:5bfc1dbe65fbac5b3e70b18826758580819a0d68e367e9b2c3626d4fe522ac89
+size 4112
--- game-service/src/main/java/com/a508/gameservice/game/service/GameRoomService.java
+++ game-service/src/main/java/com/a508/gameservice/game/service/GameRoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1a46bb90458a37d73a945585a8cdd7061ecf3cbb917f2a6f0fa033c8c87ba0e7
-size 17104
+oid sha256:e6a0a4c75bee32c22153a08afa634f71d69aa3e73ae085fc087a44b946a3223b
+size 17123
