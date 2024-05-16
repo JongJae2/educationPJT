1e21911832310a4fb7e773f3ae4d0f8081f47880
이지원 <dlwldnjs950@naver.com>
2024-05-16 14:10:44 +0900

    fix(fe): 응답 반영
    Former-commit-id: 629d4e333adf4d5ec6d021230e316733937cdea8

--- client/src/app/(main)/(home)/mypage/record/_components/CalendarComponent.tsx
+++ client/src/app/(main)/(home)/mypage/record/_components/CalendarComponent.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cdcd60b612128406279b66d6928036d9e4190bbf7e4d63b4dac1a954642f5ec7
-size 2381
+oid sha256:7b1658d8ed20936edc9d1b6026cd5f245fb7648c91f1205b9a9ab14d028bf283
+size 2483
