39f1c226484c53eae53d210a48d7d38b84c1fd0d
권정훈 <dhtmxk8134@naver.com>
2024-05-16 17:49:18 +0900

    fix(fe): 인터셉터 수정
    Former-commit-id: 8522336ea5319c550e29930440f164b7fa5b4ace

--- client/src/utils/interceptor.ts
+++ client/src/utils/interceptor.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:de8393bda91da31b479743ebac44623e2915fbc5e992f429b2208a6273bac7f5
-size 2603
+oid sha256:23071e5dfc17191b5aa403c9320b9c25b6dccfd118621f73b7826a198cfbb5c8
+size 2628
