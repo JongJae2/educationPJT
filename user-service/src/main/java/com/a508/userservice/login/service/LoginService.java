9402509e274f7d6a179bbc6bd0419fc42713dd79
최정윤 <janet971015@naver.com>
2024-05-16 14:12:49 +0900

    refactor(be): 로그 찍기
    Former-commit-id: 2181f6779f5c27da8524e38648a6f153a587658c

--- user-service/src/main/java/com/a508/userservice/login/service/LoginService.java
+++ user-service/src/main/java/com/a508/userservice/login/service/LoginService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d9ed3d7fc21659994a6f1c244a0c2892da65a00aaaf516440f6ecc28df8b7ca2
-size 9069
+oid sha256:31e792c3f4419bad2c290769e5ccb356118e9105601455112072ba97cfa08cb4
+size 9122
