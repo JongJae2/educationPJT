3d9584ea7ac6470bd4158d7c2c9f403ed9fc47b1
최정윤 <janet971015@naver.com>
2024-05-16 14:12:49 +0900

    refactor(be): 로그 찍기
    Former-commit-id: 3cd6924ca79970bc3dcd621d1d7f63cf757728f1

--- user-service/src/main/java/com/a508/userservice/login/controller/LoginController.java
+++ user-service/src/main/java/com/a508/userservice/login/controller/LoginController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f0e60bde77066de36472fb38e0a9bfc8c677b057e408badf05a3f6c16ef84451
-size 1411
+oid sha256:a6c89a8cd4e10dee33150c8402cabf760b73ed8b4781d0b75b2ff93dc3c5705b
+size 1458
