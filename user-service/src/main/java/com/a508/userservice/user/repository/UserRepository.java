df410da8c0bf05f1abb7127e7b59e69f8a549b00
최정윤 <janet971015@naver.com>
2024-05-16 15:12:43 +0900

    refactor(be): 도메인 변경
    Former-commit-id: f825d0037590101a87c39860868c4280279a6598

--- user-service/src/main/java/com/a508/userservice/user/repository/UserRepository.java
+++ user-service/src/main/java/com/a508/userservice/user/repository/UserRepository.java
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:0860fb3d09c69cc5d5588863eee462fab6e6f278dbdcdf4fd9a7f68708b9d29c
-size 391
+package com.a508.userservice.user.repository;
+
+import com.a508.userservice.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+	User findByGoogleId(String googleId);
+
+	@Query("SELECT MAX(u.id) FROM User u")
+	Integer findMaxUserId();
+
+}
