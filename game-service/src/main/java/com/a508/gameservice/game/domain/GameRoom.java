4351633b98256d31a071a5a6d836bd7255e65fb0
최정윤 <janet971015@naver.com>
2024-05-16 10:35:29 +0900

    refactor(be): 게임 상태 명확히 변경
    Former-commit-id: 315ee96e2d189467436d5f689b9a71936751c55e

--- game-service/src/main/java/com/a508/gameservice/game/domain/GameRoom.java
+++ game-service/src/main/java/com/a508/gameservice/game/domain/GameRoom.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dc6ad4e449aed0cf94085acc529eb5eed30556f228ad11174963eb4b3d84566a
-size 895
+oid sha256:aaba7c9b9eb67fea41b10c411b0263fbbe1f98c51351681135aa4db444eaf7a2
+size 911
