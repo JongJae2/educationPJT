e5dd738fcd2f524271b723ade4438df973a11c47
최정윤 <janet971015@naver.com>
2024-05-16 16:31:16 +0900

    fix(be): cookie
    Former-commit-id: b9bdbe84201986736250db04d5d089f6257fe2c4

--- user-service/src/main/java/com/a508/userservice/login/service/LoginService.java
+++ user-service/src/main/java/com/a508/userservice/login/service/LoginService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:24e05b1bd90bfe3659a25d4c2f2b6eefc36255a35b9e482baf66483bea72f5e8
-size 9437
+oid sha256:59b2938f11cb1dccef302cde0fd80a42cae1a235430366e4aeaa719ccce38b57
+size 8785
