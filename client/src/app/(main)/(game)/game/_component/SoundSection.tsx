964fb25f19c5800f942ff0f909e80f21938f3a98
하재률 <wofbf2@gmail.com>
2024-05-16 14:35:55 +0900

    fix(fe): 빌드에러 수정
    Former-commit-id: 1c1e873db704af7869ea26b20e69ef492955a578

--- client/src/app/(main)/(game)/game/_component/SoundSection.tsx
+++ client/src/app/(main)/(game)/game/_component/SoundSection.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:2e202669da505293c36d0454548ed45f7730a9d434f987ec8058d17bed39d4df
+oid sha256:6d2d2306404b731afa957bf35a19a7aef942e73e49a39822b678d3ae2ae5c4b5
 size 1182
