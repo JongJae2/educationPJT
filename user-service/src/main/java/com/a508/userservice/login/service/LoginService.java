d6dde3bafd14cf2f367d3080a56153ad0eca8d66
최정윤 <janet971015@naver.com>
2024-05-16 15:06:43 +0900

    refactor(be): 도메인 변경
    Former-commit-id: ecf7e6a3218698aace447c01ea3cf1275fb58499

--- user-service/src/main/java/com/a508/userservice/login/service/LoginService.java
+++ user-service/src/main/java/com/a508/userservice/login/service/LoginService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6fddfdbcc89b517156e2bd2ee28e28f90ce02479a1f5cb3be5d98089e792508d
+oid sha256:95fd1d67d70a6ee3ee03f95aaf505ed1a028fed9632d7e9fc2c5321f7a693eb3
 size 9234
