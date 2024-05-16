9d13cc5a8bcadfd07d0d138313c1b15a7dc3fb8d
하재률 <wofbf2@gmail.com>
2024-05-16 17:32:07 +0900

    fix(fe): 배경음 on
    Former-commit-id: 5ce7cc6b73c4388e9e671d49b8602bfc2a103ff3

--- client/src/stores/sound-control.ts
+++ client/src/stores/sound-control.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d74d0d5722085ea691ff653e417b86020cbda04f802d982ca2a72351fe82af07
-size 442
+oid sha256:871f5450363bc0107dd149a1efbee8054246064f082eeb540b401f4fc6430254
+size 441
