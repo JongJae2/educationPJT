0166e2566292c06ce28ff98bbc67b56c2dd7fef4
최정윤 <janet971015@naver.com>
2024-05-17 19:54:07 +0900

    refactor(be): 게임 40초로 수정
    Former-commit-id: 9c08bf2a312d931b48afd3a229c613156c349665

--- game-service/src/main/java/com/a508/gameservice/game/service/SchedulerService.java
+++ game-service/src/main/java/com/a508/gameservice/game/service/SchedulerService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b17587bbbada047c24f1e2f2a45bc69de46c0198df4fc29c52ed4baf8a92d318
+oid sha256:86b027267ecc89e5841ae96a63b54a692f830170f1ca46d99ec100c2ca2cb18a
 size 3089
