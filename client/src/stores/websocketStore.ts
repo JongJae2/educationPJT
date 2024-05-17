05e62e0524778c4036a517d15298171c6de2aa84
하재률 <wofbf2@gmail.com>
2024-05-17 19:31:52 +0900

    fix(fe): 퀴즈 시간 40초로 변경
    Former-commit-id: bd9b826a12c7b311b0cea5fb1a6f1b3f8e76b6a7

--- client/src/stores/websocketStore.ts
+++ client/src/stores/websocketStore.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cfbd5079e44b64a28157c982325d7c573e30f034d5d218cb230150d5fad14391
+oid sha256:6d5018a027cb13d829f21946e45fb902b5dc0ca5ac23be5cf7b09908a8e569cf
 size 7135
