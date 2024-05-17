b4f4d4c4faf3d8f57cc636007a359bb9a01c5282
권정훈 <righthun.kwon@gmail.com>
2024-05-17 15:19:00 +0900

    fix(fe): 인스턴스 수정
    Former-commit-id: 1c4a766bc6b731bf4d15aa087f8f9e33d4d1ec71

--- client/src/utils/interceptor.ts
+++ client/src/utils/interceptor.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a5aeaece143dd83208899edec3a7020f1b7c4766dd7737b8b56d885843dfb163
-size 3399
+oid sha256:59baa6864a46e02cba202beca0d963bbc4c3bf59f8426731639ac10067a58d24
+size 3816
