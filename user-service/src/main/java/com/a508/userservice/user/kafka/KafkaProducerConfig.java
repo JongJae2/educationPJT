7f446f147bab9ffcf4143d7cfd0f752c0b669d0f
이종재 <149658209+JongJae2@users.noreply.github.com>
2024-05-19 07:03:53 +0900

    fix(be): 경로 재설정
    Former-commit-id: 19dc19c8842ed661c4003d95295c5a954955dfb4

--- user-service/src/main/java/com/a508/userservice/user/kafka/KafkaProducerConfig.java
+++ user-service/src/main/java/com/a508/userservice/user/kafka/KafkaProducerConfig.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05ab49ed6dd4f86d4d31d0541139074289b8e502ec5bd0d244dde6e0ca934429
-size 1249
+oid sha256:f2d8f9cf1274502b9480a7113b8a6b4ef88a5d510ed6327f0fbd072c0f5e0646
+size 1263
