2d1efde4d3aa6844c1855f803822aa82d418d089
이지원 <dlwldnjs950@naver.com>
2024-05-18 22:57:17 +0900

    feat(fe): SSE 받을 eventsource 객체 생성 코드
    Former-commit-id: 679430439cef33ed3781d4db9454dcac912c0581

--- client/src/app/(main)/_components/NavMenu.tsx
+++ client/src/app/(main)/_components/NavMenu.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9a7232df1838cb1716fba2366bedf4a1ab6f2f9f31e4bd7515ec2c3953f5a9c2
-size 14027
+oid sha256:2d2e3542463570b7ab77d04f917c8cd680a271840d7b5d8c286c58bd6a4debe2
+size 14711
