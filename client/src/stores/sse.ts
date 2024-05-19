8080fa8b9036810c9d3dbcdd09d39dbdec300de8
이지원 <dlwldnjs950@naver.com>
2024-05-19 17:43:12 +0900

    fix(fe): 초기값 변경
    Former-commit-id: ed43b37d8dfbf4395db40d7fcd509e22decea61b

--- client/src/stores/sse.ts
+++ client/src/stores/sse.ts
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b7122a3140a781bd14ebc610630fa2235309d60c03470ba5fed6c66b7a9f8deb
-size 1625
+oid sha256:55d507d1010f54336243bb98ac1be301c3c6bbc4626391229c1068d1a128d950
+size 1646
