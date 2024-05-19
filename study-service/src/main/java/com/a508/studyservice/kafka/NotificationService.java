c3aa50b811de34e92d72d54ceb1ac7e7655a0d40
이종재 <149658209+JongJae2@users.noreply.github.com>
2024-05-19 19:04:53 +0900

    fix(be): Sse 로직수정
    Former-commit-id: 0b0a96ebf1903e331c3f613ce61ddc391b1ac4a5

--- study-service/src/main/java/com/a508/studyservice/kafka/NotificationService.java
+++ study-service/src/main/java/com/a508/studyservice/kafka/NotificationService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:4430a8e29a01b65b5aa900c3d5f274883c4b402ab22541a5201217109a53e659
-size 4555
+oid sha256:eb9fcde58bd4b39378ae0bc6ff6184455d696b5ac7773fbe023fc7d67c624ff6
+size 4406
