ebeae11dc9ca721a10ed1662330ba01230a7cbcc
이지원 <dlwldnjs950@naver.com>
2024-05-19 19:29:43 +0900

    feat(fe): 토큰 저장 및 이벤트 수신 객체 생성
    Former-commit-id: 5549525d17176be2bd5703160e9a4c92f4cd2395

--- client/src/app/(intro)/login/authentication/_components/Authentication.tsx
+++ client/src/app/(intro)/login/authentication/_components/Authentication.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:022c1ce64d780a8def9ad018a011d35cd1ca9ec5ea5a32abea10912d733032d4
-size 1825
+oid sha256:4ca2b3d1c5248c4133f4361b3993e06d0b9b5d7abf6c7f71e60888ac403a88e3
+size 1706
