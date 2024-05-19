7002e65a7ecefabd48615d3315cb91e10feca0e8
이종재 <149658209+JongJae2@users.noreply.github.com>
2024-05-19 09:09:04 +0900

    feat(be): 카프카 컨슈머, Sse, 맞춤형 알림 스케쥴링 구현
    Former-commit-id: b073cfe65580b67b157556f9fda7e50a3f7de940

---
+++ study-service/src/main/java/com/a508/studyservice/kafka/NotificationConsumer.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:09a24c7e62910121fcd79264d4e1e882a1cb1cbfdf0035b4c74b2b9683b441b8
+size 688
---
+++ study-service/src/main/java/com/a508/studyservice/kafka/NotificationService.java
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4430a8e29a01b65b5aa900c3d5f274883c4b402ab22541a5201217109a53e659
+size 4555
