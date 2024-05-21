73eb43b9182aa449fe7977bd5e9f836e750eda59
최정윤 <janet971015@naver.com>
2024-05-21 10:02:30 +0900

    fix(be): 게임 버그 수정
    Former-commit-id: 061d9581f748cb5872da381228e9e723ba661c4a

--- game-service/src/main/java/com/a508/gameservice/game/service/GameRoomService.java
+++ game-service/src/main/java/com/a508/gameservice/game/service/GameRoomService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6a0a4c75bee32c22153a08afa634f71d69aa3e73ae085fc087a44b946a3223b
-size 17123
+oid sha256:f26eb9db2066aa39ad13f79c6927d4ec9e9e0e56156fc9a7effc111c36810494
+size 17199
