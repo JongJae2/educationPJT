ae60b9bfc27022028d404e364abcf0c5aa679b58
강지헌 <wlgjs0458@naver.com>
2024-05-23 13:33:22 +0900

    fix(be): 순위 처리 로직 수정
    Former-commit-id: 3e0cb8f832e8a22097b49801e050a3520e6df7f6

--- recommend/recommend/app.py
+++ recommend/recommend/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b1c15724f8c1b5ab834bf73b4b4d9ac62f30d43a2a48a46b3c9d465958073c9
-size 4623
+oid sha256:4fad16f6f3c699191137950d833e38b8befd979997bc5be44e0101102aec0322
+size 4910
