6ddea86a3ddfef3e03c50d3c1608bc70cb239958
이지원 <dlwldnjs950@naver.com>
2024-05-23 13:08:34 +0900

    design(fe): 소개 페이지 레이아웃 변경 (좌우 상하 카드)
    Former-commit-id: ffc4e59e9ff0c6687ce381f66b82e5c7a19d117c

--- client/src/app/(intro)/(about)/_component/CardContainer.tsx
+++ client/src/app/(intro)/(about)/_component/CardContainer.tsx
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5627cbfe0000029422124887f357cb5d3dad8f6abed6b4bfc6ac2a0682941a7e
-size 1375
+oid sha256:8553d1247f73b468872d6cc543ced41fa8339b85ccfdbe3b7a66d0d914b53d28
+size 1384
