5b966a78bd8af3ba65d73aa90abc9e31e53941e1
이종재 <ljj452@naver.com>
2024-05-23 17:42:24 +0900

    fix(be): 오류 가능성 수정
    Former-commit-id: 79ad4d839753201fc57fbbc2a0886e564cd3cba4

--- study-service/src/main/java/com/a508/studyservice/service/TodayLearningScheduler.java
+++ study-service/src/main/java/com/a508/studyservice/service/TodayLearningScheduler.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fe51e48b8025f5d5f266ed876360d898286b98b330434c6dd62fb406a403e15b
-size 8531
+oid sha256:6cc9c305772ef35c6edce52c746edf9135bdfe9edfd52a3e59066ec10c85ed5b
+size 8420
