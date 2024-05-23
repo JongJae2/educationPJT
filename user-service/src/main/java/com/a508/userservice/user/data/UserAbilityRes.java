c3cd613851e77e78fa84819fbcaba1f0b73d2e5f
이종재 <ljj452@naver.com>
2024-05-23 18:05:39 +0900

    fix(be): dto 수정
    Former-commit-id: f1d4c0b8f328129652d8bfe9de112ac701f076ee

--- user-service/src/main/java/com/a508/userservice/user/data/UserAbilityRes.java
+++ user-service/src/main/java/com/a508/userservice/user/data/UserAbilityRes.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d87e9578b0b349034599e52ab4af47a984ab3f485ed6d6fa08c3eb088dac0048
-size 402
+oid sha256:fb1347fad5e21b2f1cb9bea0e65d20fa78150d4385629581e4027de5b97b05a0
+size 397
