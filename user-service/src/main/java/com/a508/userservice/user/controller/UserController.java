7635613bdf644bd9ddf0ef088489a7081df36b57
이종재 <ljj452@naver.com>
2024-05-23 18:05:39 +0900

    fix(be): dto 수정
    Former-commit-id: 883a7bd681e859c7c0480a5cb316f85d4f4c70c7

--- user-service/src/main/java/com/a508/userservice/user/controller/UserController.java
+++ user-service/src/main/java/com/a508/userservice/user/controller/UserController.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72cc3c679e4173328f49e14ccf4bd163de8c7e25e86cac473c5b054bb8b8b79e
-size 7108
+oid sha256:12925a420ddbc29411ac0795f568fdb6a030b600802d2856a9effa79cc01fa86
+size 7322
