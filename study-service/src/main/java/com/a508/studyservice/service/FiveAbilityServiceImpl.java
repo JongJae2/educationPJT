d8ee5386349b96d11e340d48db8f776f97815667
이종재 <ljj452@naver.com>
2024-05-23 17:55:02 +0900

    fix(be): 계산 로직 수정
    Former-commit-id: b476683c1808186b6216796533eadc1087a6dea7

--- study-service/src/main/java/com/a508/studyservice/service/FiveAbilityServiceImpl.java
+++ study-service/src/main/java/com/a508/studyservice/service/FiveAbilityServiceImpl.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d1577d83904e35e3d33f4c216c0844a9b5daaaaea8920ec734c53eb2def207c9
-size 2255
+oid sha256:a24bf54bcf1c56daf6250466dcb25a2f0d40f8aab749d1160ee59872ac2acd30
+size 2245
