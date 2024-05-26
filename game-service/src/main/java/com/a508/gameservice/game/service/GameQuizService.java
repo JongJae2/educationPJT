68ac3036ec27a2a9abe346c5539f42faca76782d
최정윤 <janet971015@naver.com>
2024-05-27 01:46:10 +0900

    refactor(be): 시연 문제 세팅
    Former-commit-id: 7a53733f177b1d78e06c4b00c14b705abfcd5637

--- game-service/src/main/java/com/a508/gameservice/game/service/GameQuizService.java
+++ game-service/src/main/java/com/a508/gameservice/game/service/GameQuizService.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b5de76d56baf250d2317fe984e0c8edd28d7e9680be41ce85de2a26dfcd6016
-size 849
+oid sha256:519faf3ac22e7ab32a55e2c6d9c6c7587e9c914c8bff820112433ce4e723801c
+size 1162
